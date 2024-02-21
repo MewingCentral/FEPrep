7ee0f321afb682c405a0fc5b89b1713a5dba248b
Caleb Rivera <calebrossr@gmail.com>
2024-02-21 05:19:52 -0500

    Remove unused imports

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,9 +1,7 @@
-import { trpcServer } from "@hono/trpc-server";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
 import type { HonoConfig } from "./config";
-import { appRouter, createTRPCContext } from ".";
 import { csrfProtection } from "./middlewares/csrf-protection";
 import { validateSession } from "./middlewares/validate-session";
 
