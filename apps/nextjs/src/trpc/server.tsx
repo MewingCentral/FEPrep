c0843abfab835b6801ad5b49d41e319d4d12ced0
Caleb Rivera <calebrossr@gmail.com>
2024-02-21 05:19:25 -0500

    Rename heads to requestHeaders

--- apps/nextjs/src/trpc/server.tsx
+++ apps/nextjs/src/trpc/server.tsx
@@ -7,13 +7,13 @@ import { createCaller, createTRPCContext } from "@feprep/api";
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
  * handling a tRPC call from a React Server Component.
  */
-const createContext = cache(async () => {
-  const heads = new Headers(headers());
-  heads.set("x-trpc-source", "rsc");
+const createTRPCServerContext = cache(async () => {
+  const requestHeaders = new Headers(headers());
+  requestHeaders.set("x-trpc-source", "rsc");
 
   return createTRPCContext({
-    headers: heads,
+    headers: requestHeaders,
   });
 });
 
-export const api = createCaller(createContext);
+export const api = createCaller(createTRPCServerContext);
