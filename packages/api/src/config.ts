1b44fb8d7d14fd8aaf144e26894bbaf0efe125a6
Caleb Rivera <calebrossr@gmail.com>
2024-02-21 20:51:16 -0500

    Update hono config

--- packages/api/src/config.ts
+++ packages/api/src/config.ts
@@ -1,24 +1,7 @@
 /* eslint-disable @typescript-eslint/consistent-type-definitions */
-import type { Context, Input } from "hono";
-
 import type { Session, User } from "@feprep/auth";
 
 export type Variables = {
   user: User | null;
   session: Session | null;
 };
-
-export type Bindings = {
-  DATABASE_URL: string;
-  DATABASE_TOKEN: string;
-};
-
-export interface HonoConfig {
-  Variables: Variables;
-  Bindings: Bindings;
-}
-
-export type HonoContext<
-  P extends string = string,
-  I extends Input = Input,
-> = Context<HonoConfig, P, I>;
