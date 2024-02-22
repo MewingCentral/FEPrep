b96a0169cb6858796a7cb499dfe0bbd2da461baa
Caleb Rivera <calebrossr@gmail.com>
2024-02-21 20:49:26 -0500

    Update auth pages

--- apps/nextjs/src/app/sign-up/page.tsx
+++ apps/nextjs/src/app/sign-up/page.tsx
@@ -1,72 +1,82 @@
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
+"use client";
 
-import { generateId, lucia, Scrypt } from "@feprep/auth";
-import { db, users } from "@feprep/db";
+import type { z } from "zod";
 
-export default async function Page() {
-  return (
-    <>
-      <h1>Create an account</h1>
-      <form action={signup}>
-        <label htmlFor="username">Username</label>
-        <input name="username" id="username" />
-        <br />
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
-        <br />
-        <button>Continue</button>
-      </form>
-    </>
-  );
-}
+import { Button } from "@feprep/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  useForm,
+} from "@feprep/ui/form";
+import { Input } from "@feprep/ui/input";
+import { SignUpSchema } from "@feprep/validators";
 
-async function signup(formData: FormData): Promise<ActionResult> {
-  "use server";
-  console.log(formData);
-  const username = formData.get("username");
-  // username must be between 4 ~ 31 characters, and only consists of lowercase letters, 0-9, -, and _
-  // keep in mind some database (e.g. mysql) are case insensitive
-  if (
-    typeof username !== "string" ||
-    username.length < 3 ||
-    username.length > 31
-  ) {
-    return {
-      error: "Invalid username",
-    };
-  }
-  const password = formData.get("password");
-  if (
-    typeof password !== "string" ||
-    password.length < 6 ||
-    password.length > 255
-  ) {
-    return {
-      error: "Invalid password",
-    };
-  }
-
-  const hashedPassword = await new Scrypt().hash(password);
-  const userId = generateId(15);
-
-  // TODO: check if username is already used
-  await db.insert(users).values({
-    id: userId,
-    username: username,
-    hashedPassword: hashedPassword,
+export default function Page() {
+  const form = useForm({
+    schema: SignUpSchema,
+    defaultValues: {
+      email: "",
+      password: "",
+    },
   });
 
-  const session = await lucia.createSession(userId, {});
-  const sessionCookie = lucia.createSessionCookie(session.id);
-  cookies().set(
-    sessionCookie.name,
-    sessionCookie.value,
-    sessionCookie.attributes,
-  );
-  return redirect("/");
-}
+  const onSubmit = async (values: z.infer<typeof SignUpSchema>) => {
+    await fetch("/api/sign-up", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(values),
+    });
+  };
 
-interface ActionResult {
-  error: string;
+  return (
+    <div className="flex flex-1 flex-col items-center justify-center">
+      <h1 className="mb-4 text-center text-xl font-semibold">Sign Up</h1>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-full max-w-[300px] space-y-8"
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="Email" autoComplete="email" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Password"
+                    type="password"
+                    autoComplete="current-password"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  );
 }
