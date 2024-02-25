ef8bfed739cd0b4b0f4f97cd1c3142a42e27ac76
Caleb Rivera <calebrossr@gmail.com>
2024-02-25 07:50:07 -0500

    Update expo app home screen

--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -1,5 +1,17 @@
+import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
-  return <SafeAreaView className=" bg-background"></SafeAreaView>;
+  return (
+    <SafeAreaView className=" bg-background">
+      <View>
+        <Text className="text-center text-4xl font-bold text-primary">
+          Welcome to expo!
+        </Text>
+        <Text className="text-center text-2xl font-bold text-primary">
+          This is a page from the expo app
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
 }
