129df6a06b0f904b132531e8c003a93311542f41
Caleb Rivera <calebrossr@gmail.com>
2024-02-25 19:53:56 -0500

    Add auth related tables to schema

--- packages/db/src/schema/users.ts
+++ packages/db/src/schema/users.ts
@@ -1,14 +1,35 @@
+import { sql } from "drizzle-orm";
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const users = sqliteTable("user", {
   id: text("id").notNull().primaryKey(),
   email: text("email").notNull().unique(),
   hashedPassword: text("hashed_password").notNull(),
+  createdAt: text("created_at").default(sql`CURRENT_TIMESTAMP`),
+  updatedAt: text("updated_at").default(sql`CURRENT_TIMESTAMP`),
 });
 
 export type SelectUser = typeof users.$inferSelect;
 
 export const sessions = sqliteTable("session", {
+  id: text("id").notNull().primaryKey(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => users.id),
+  expiresAt: integer("expires_at").notNull(),
+});
+
+export const emailVerificationTokens = sqliteTable("email_verification_token", {
+  id: text("id").notNull().primaryKey(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => users.id),
+  expiresAt: integer("expires_at").notNull(),
+  email: text("email").notNull(),
+  code: text("code").notNull(),
+});
+
+export const passwordResetTokens = sqliteTable("password_reset_token", {
   id: text("id").notNull().primaryKey(),
   userId: text("user_id")
     .notNull()
