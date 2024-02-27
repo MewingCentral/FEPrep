bbbebb76e4ac23041b4d7a59a17e14684dc71090
Luis Infante <113048160+LuisMInfante@users.noreply.github.com>
2024-02-26 21:49:33 -0500

    Updated Sign-In fields, added link to Sign-Up

--- apps/nextjs/src/app/sign-in/page.tsx
+++ apps/nextjs/src/app/sign-in/page.tsx
@@ -3,17 +3,18 @@
 import { Button } from "@feprep/ui/button";
 import { Input } from "@feprep/ui/input";
 import { Label } from "@feprep/ui/label";
+import Link from "next/link";
 
 export default function Page() {
   return (
     <div className="flex flex-1 flex-col items-center justify-center">
       <h1 className="mb-4 text-center text-xl font-semibold">Sign in</h1>
       <form className="grid w-full max-w-[300px] gap-1.5">
-        <Label htmlFor="email">Email</Label>
+        <Label htmlFor="email">NID</Label>
         <Input
           id="email"
-          name="email"
-          placeholder="Email"
+          name="Email"
+          placeholder="jd123456"
           autoComplete="email"
           type="email"
         />
@@ -27,6 +28,9 @@ export default function Page() {
         />
         <Button variant="primary">Sign In</Button>
       </form>
+      <p className="mt-4 text-center text-sm font-regular">Don&apos;t have an account?  
+        <Link href="/sign-up" className="font-semibold"> Register</Link>
+      </p>
     </div>
   );
 }
