09d1eda70b9f6d1207641c23323eb48b8193b8cd
Caleb Rivera <calebrossr@gmail.com>
2024-02-28 11:12:28 -0500

    Clean up sign up

--- packages/api/src/routes/sign-up.ts
+++ packages/api/src/routes/sign-up.ts
@@ -1,8 +1,11 @@
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
+import { createDate } from "oslo";
+import { alphabet, generateRandomString } from "oslo/crypto";
 
-import { generateId, lucia, Scrypt } from "@feprep/auth";
-import { db, users } from "@feprep/db";
+import { generateId, lucia, Scrypt, TimeSpan } from "@feprep/auth";
+import { db, emailVerificationCodes, eq, users } from "@feprep/db";
+import { renderEmailVerification, resend } from "@feprep/transactional";
 import { SignUpSchema } from "@feprep/validators";
 
 export const signUp = new Hono().post(
@@ -10,6 +13,8 @@ export const signUp = new Hono().post(
   zValidator("json", SignUpSchema),
   async (c) => {
     const { email, password } = c.req.valid("json");
+    console.log("email", email);
+    console.log("password", password);
 
     // Check if user already exists
     const existingUser = await db.query.users.findFirst({
@@ -29,12 +34,13 @@ export const signUp = new Hono().post(
       hashedPassword: hashedPassword,
     });
 
-    // const verificationCode = await generateEmailVerificationCode(userId, email);
-    // await sendMail({
-    //   to: email,
-    //   subject: "Verify your account",
-    //   body: renderVerificationCodeEmail({ code: verificationCode }),
-    // });
+    const verificationCode = await generateEmailVerificationCode(userId, email);
+    await resend.emails.send({
+      to: email,
+      from: "team@feprep.org",
+      subject: "Verify your email",
+      html: renderEmailVerification(verificationCode),
+    });
 
     const session = await lucia.createSession(userId, {});
     c.header("Set-Cookie", lucia.createSessionCookie(session.id).serialize(), {
@@ -44,3 +50,20 @@ export const signUp = new Hono().post(
     return c.json({ session: session.id, userId: userId });
   },
 );
+
+export async function generateEmailVerificationCode(
+  userId: string,
+  email: string,
+): Promise<string> {
+  await db
+    .delete(emailVerificationCodes)
+    .where(eq(emailVerificationCodes.userId, userId));
+  const code = generateRandomString(8, alphabet("0-9")); // 8 digit code
+  await db.insert(emailVerificationCodes).values({
+    userId,
+    email,
+    code,
+    expiresAt: createDate(new TimeSpan(10, "m")).getSeconds(), // 10 minutes
+  });
+  return code;
+}
