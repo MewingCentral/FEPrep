13c89bd32472e0c27bd2016ee5e65c0da24703d0
Caleb Rivera <calebrossr@gmail.com>
2024-02-28 01:11:09 -0500

    Add sign in schem

--- packages/validators/src/index.ts
+++ packages/validators/src/index.ts
@@ -1,7 +1,12 @@
 import { z } from "zod";
 
 export const SignUpSchema = z.object({
-  email: z.string().email(),
+  email: z.string().email("Invalid email address"),
   // TODO: make this more secure
-  password: z.string().min(8),
+  password: z.string().min(8, "Password must be at least 8 characters"),
+});
+
+export const SignInSchema = z.object({
+  email: z.string(),
+  password: z.string(),
 });
