a9a148e85514be25e66e445838742100243e544e
Caleb Rivera <calebrossr@gmail.com>
2024-02-29 22:18:03 -0500

    Reorganize api folder

--- packages/api/src/config.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/consistent-type-definitions */
-import type { Session, User } from "@feprep/auth";
-
-export type Variables = {
-  user: User | null;
-  session: Session | null;
-};
