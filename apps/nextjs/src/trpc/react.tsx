5e917d20b2ef2e265291c5892ec03ef860f8fb13
Caleb Rivera <calebrossr@gmail.com>
2024-02-29 22:18:24 -0500

    Use stable http batch link

--- apps/nextjs/src/trpc/react.tsx
+++ apps/nextjs/src/trpc/react.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { loggerLink, unstable_httpBatchStreamLink } from "@trpc/client";
+import { httpBatchLink, loggerLink } from "@trpc/client";
 import { createTRPCReact } from "@trpc/react-query";
 import SuperJSON from "superjson";
 
@@ -21,7 +21,7 @@ export function TRPCReactProvider(props: { children: React.ReactNode }) {
             process.env.NODE_ENV === "development" ||
             (op.direction === "down" && op.result instanceof Error),
         }),
-        unstable_httpBatchStreamLink({
+        httpBatchLink({
           transformer: SuperJSON,
           url: getBaseUrl() + "/api/trpc",
           async headers() {
