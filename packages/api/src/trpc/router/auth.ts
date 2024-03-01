815aff8b9b1432036c32ea93b8a2a811a69fe4ee
Caleb Rivera <calebrossr@gmail.com>
2024-02-29 21:03:28 -0500

    Move auth helpers

--- packages/api/src/trpc/router/auth.ts
+++ packages/api/src/trpc/router/auth.ts
@@ -1,8 +1,9 @@
 import { cookies } from "next/headers";
 import { TRPCError } from "@trpc/server";
 
-import { generateId, lucia, Scrypt } from "@feprep/auth";
+import { generateId, lucia, Scrypt, TimeSpan } from "@feprep/auth";
 import {
+  db,
   emailVerificationCodes,
   eq,
   passwordResetTokens,
@@ -22,8 +23,8 @@ import {
 } from "@feprep/validators";
 
 import { createTRPCRouter, protectedProcedure, publicProcedure } from "..";
-import { generatePasswordResetToken } from "../../routes/forgot-password";
-import { generateEmailVerificationCode } from "../../routes/sign-up";
+import { createDate } from "oslo";
+import { alphabet, generateRandomString } from "oslo/crypto";
 
 export const authRouter = createTRPCRouter({
   signUp: publicProcedure
@@ -266,3 +267,36 @@ export const authRouter = createTRPCRouter({
     return ctx.user;
   }),
 });
+
+export async function generatePasswordResetToken(
+  userId: string,
+): Promise<string> {
+  await db
+    .delete(passwordResetTokens)
+    .where(eq(passwordResetTokens.userId, userId));
+  const tokenId = generateId(40);
+  await db.insert(passwordResetTokens).values({
+    id: tokenId,
+    userId,
+    expiresAt: createDate(new TimeSpan(2, "h")).getSeconds(),
+  });
+  return tokenId;
+}
+
+export async function generateEmailVerificationCode(
+  userId: string,
+  email: string,
+): Promise<string> {
+  await db
+    .delete(emailVerificationCodes)
+    .where(eq(emailVerificationCodes.userId, userId));
+  const code = generateRandomString(8, alphabet("0-9")); // 8 digit code
+  await db.insert(emailVerificationCodes).values({
+    userId,
+    email,
+    code,
+    expiresAt: createDate(new TimeSpan(10, "m")).getSeconds(), // 10 minutes
+  });
+  return code;
+}
+
