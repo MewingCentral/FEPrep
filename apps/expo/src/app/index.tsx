47d6a1045d1be6651ff5e6fa8aef42b2f67c37b8
anna-sage-local <anna.macinnis109@gmail.com>
2024-03-01 09:06:41 -0500

    buttons and updated text

--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -1,16 +1,24 @@
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <SafeAreaView className=" bg-background">
+    <SafeAreaView className="bg-background">
       <View>
         <Text className="text-center text-4xl font-bold text-primary">
-          Welcome to expo!
+          FEPrep
         </Text>
         <Text className="text-center text-2xl font-bold text-primary">
-          This is a page from the expo app
+          A new way to prepare for the Foundation Exam.
         </Text>
+        <View className="flex flex-col gap-5">
+          <Button title="Start Practicing" color="#0F172A" 
+            accessibilityLabel="Access the dashboard without logging in.">
+          </Button>
+          <Button title="Create an Account" color="#64748B"
+            accessibilityLabel="Create an account to access all FEPrep features.">
+          </Button>
+        </View>
       </View>
     </SafeAreaView>
   );
