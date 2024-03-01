d1793a1f74417ffb87d9d0e5cd04ceb2f4089559
Caleb Rivera <calebrossr@gmail.com>
2024-02-29 20:52:24 -0500

    register auth router

--- packages/api/src/trpc/router/index.ts
+++ packages/api/src/trpc/router/index.ts
@@ -1,9 +1,11 @@
 import { createTRPCRouter, publicProcedure } from "..";
+import { authRouter } from "./auth";
 import { usersRouter } from "./users";
 
 export const appRouter = createTRPCRouter({
   hello: publicProcedure.query(() => "hewwo world"),
   users: usersRouter,
+  auth: authRouter,
 });
 
 export type AppRouter = typeof appRouter;
