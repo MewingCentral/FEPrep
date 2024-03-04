4381cd5a1b71780205ccb31b2425e22021a1eb85
Pharit Smitasin <pharitsmitasin@gmail.com>
2024-03-04 18:00:42 -0500

    remove api import and user const

--- apps/nextjs/src/app/dashboard/page.tsx
+++ apps/nextjs/src/app/dashboard/page.tsx
@@ -1,8 +1,8 @@
-import { api } from "~/trpc/server";
+// import { api } from "~/trpc/server";
 import { Dashboard } from "./dashboard";
 
 export default async function Page() {
-  const user = await api.auth.getUser();
+  // const user = await api.auth.getUser();
 
   // return <Dashboard user={user} />;
   return <Dashboard />;
