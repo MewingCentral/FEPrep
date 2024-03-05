b27d1a4da0249adfe3c851734b05496e492852e6
anna-sage-local <anna.macinnis109@gmail.com>
2024-03-05 15:05:33 -0500

    Small format adjustments

--- apps/expo/src/app/_layout.tsx
+++ apps/expo/src/app/_layout.tsx
@@ -2,47 +2,34 @@ import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
 import { TRPCProvider } from "~/utils/api";
-
-import "../styles.css";
-
-import { useColorScheme } from "nativewind";
-
+// import { useColorScheme } from "nativewind";
 import Colors from "~/utils/colors";
 
-// This is the main layout of the app
-// It wraps your pages with the providers they need
-export default function RootLayout() {
-  const { colorScheme } = useColorScheme();
+export default function dashboardLayout() {
+  // const { colorScheme } = useColorScheme();
   return (
     <TRPCProvider>
       {/*
-          The Stack component displays the current page.
-          It also allows you to configure your screens 
-        */}
+            The Stack component displays the current page.
+            It also allows you to configure your screens 
+          */}
       <Stack
         screenOptions={{
           headerStyle: {
-<<<<<<< HEAD
-            backgroundColor:
-              colorScheme == "dark" ? Colors.dark_sec : Colors.light_sec,
-=======
-            backgroundColor: colorScheme == "dark" ? "#1e293b" : "#f1f5f9",
->>>>>>> 32ab6d0 (Temporarily adjusted styling)
+            backgroundColor: Colors.dark_sec,
           },
           contentStyle: {
-            backgroundColor: colorScheme == "dark" ? "#020817" : "#F8FAFC",
+            backgroundColor: Colors.dark_bg,
           },
-          headerTintColor:
-            colorScheme == "dark"
-              ? Colors.dark_primary_text
-              : Colors.light_primary_text,
         }}
-<<<<<<< HEAD
-      ></Stack>
-=======
       >
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerShown: false,
+          }}
+        />
       </Stack>
->>>>>>> 32ab6d0 (Temporarily adjusted styling)
       <StatusBar />
     </TRPCProvider>
   );
