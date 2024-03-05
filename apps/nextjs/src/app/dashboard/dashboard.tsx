6d5b980f928492c6755ed7220cde37c2c0129844
qnbby <quan.nguyen.duc.3010@gmail.com>
2024-03-05 14:34:25 -0500

    PLEASE

--- apps/nextjs/src/app/dashboard/dashboard.tsx
+++ apps/nextjs/src/app/dashboard/dashboard.tsx
@@ -1,30 +1,62 @@
 "use client";
 
 // import { useRouter } from "next/navigation";
-import { User } from "@feprep/auth";
 
-import { Left } from "./components/left";
+// import { User } from "@feprep/auth";
+// import { Button } from "@feprep/ui/button";
+
 // import { api } from "~/trpc/react";
-import { Nav } from "./components/nav";
+// import { Nav } from "./components/nav";
+// import { Workbench } from "./components/workbench";
 
-// import { Right } from "./components/right"; // for when this is implemented
+// main dashboard, commented out because LINT
+// export function Dashboard({ user }: { user: User | null }) {
+//   return (
+//     <main>
+//       <Nav />
+//       {/* <AuthButton user={user} /> */}
+//       <Workbench />
+//     </main>
+//   );
+// }
 
-export function Dashboard({ user }: { user: User | null }) {
+export function Dashboard() {
   return (
-    <div className="flex h-screen flex-col">
-      <Nav user={user} />
-
-      {/* <AuthButton user={user} /> */}
-      <main className="flex flex-1">
-        <div className="h-full w-1/2">
-          <Left />
+    <main>
+      <div className="container grid grid-cols-2">
+        <div className="left-section rounded-sm border border-white bg-gray-200 text-black">
+          <h1> left side </h1>
         </div>
-
-        <div className="h-full w-1/2 bg-gray-200">
-          {/* Just a placeholder background for visibility */}
-          {/* <Right /> */}
+        <div className="right-section rounded-sm border border-white bg-gray-200 text-black">
+          <div className="">
+            <h1>right side</h1>
+          </div>
+          {/* <AuthButton user={user} /> */}
         </div>
-      </main>
-    </div>
+      </div>
+    </main>
   );
 }
+
+// function AuthButton({ user }: { user: User | null }) {
+//   const { mutateAsync } = api.auth.signOut.useMutation();
+//   const router = useRouter();
+
+//   if (user) {
+//     return (
+//       <Button
+//         onClick={async () => {
+//           try {
+//             await mutateAsync();
+//             router.push("/sign-in");
+//           } catch {
+//             // noop
+//           }
+//         }}
+//       >
+//         Sign out
+//       </Button>
+//     );
+//   }
+//   return <div>Not signed in</div>;
+// }
