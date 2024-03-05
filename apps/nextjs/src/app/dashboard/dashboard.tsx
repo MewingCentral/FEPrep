6e24ae994040a88a0358fe610c16fe31ea19676c
qnbby <quan.nguyen.duc.3010@gmail.com>
2024-03-05 14:21:49 -0500

    please pass checks please please pleasep aselpealepsaelasp

--- apps/nextjs/src/app/dashboard/dashboard.tsx
+++ apps/nextjs/src/app/dashboard/dashboard.tsx
@@ -9,54 +9,55 @@ import { api } from "~/trpc/react";
 import { Nav } from "./components/nav";
 import { Workbench } from "./components/workbench";
 
-export function Dashboard({ user }: { user: User | null }) {
-  return (
-    <main>
-      <Nav />
-      {/* <AuthButton user={user} /> */}
-      <Workbench />
-    </main>
-  );
-}
-
-// export function Dashboard() {
+// main function is here comment out for lint
+// export function Dashboard({ user }: { user: User | null }) {
 //   return (
 //     <main>
-//       <div className="container grid grid-cols-2">
-//         {/* <div className="left-section border border-white rounded-sm bg-gray-200 text-black">
-//           <h1> left side </h1>
-//         </div> */}
-//         <div className="right-section border border-white rounded-sm bg-gray-200 text-black">
-//           <div className="">
-//             <h1>right side</h1>
-//           </div>
-//           {/* <AuthButton user={user} /> */}
-//         </div>
-//       </div>
-
+//       <Nav />
+//       {/* <AuthButton user={user} /> */}
+//       <Workbench />
 //     </main>
 //   );
 // }
 
-function AuthButton({ user }: { user: User | null }) {
-  const { mutateAsync } = api.auth.signOut.useMutation();
-  const router = useRouter();
+export function Dashboard() {
+  return (
+    <main>
+      <div className="container grid grid-cols-2">
+        {/* <div className="left-section border border-white rounded-sm bg-gray-200 text-black">
+          <h1> left side </h1>
+        </div> */}
+        <div className="right-section border border-white rounded-sm bg-gray-200 text-black">
+          <div className="">
+            <h1>right side</h1>
+          </div>
+          {/* <AuthButton user={user} /> */}
+        </div>
+      </div>
 
-  if (user) {
-    return (
-      <Button
-        onClick={async () => {
-          try {
-            await mutateAsync();
-            router.push("/sign-in");
-          } catch {
-            // noop
-          }
-        }}
-      >
-        Sign out
-      </Button>
-    );
-  }
-  return <div>Not signed in</div>;
+    </main>
+  );
 }
+
+// function AuthButton({ user }: { user: User | null }) {
+//   const { mutateAsync } = api.auth.signOut.useMutation();
+//   const router = useRouter();
+
+//   if (user) {
+//     return (
+//       <Button
+//         onClick={async () => {
+//           try {
+//             await mutateAsync();
+//             router.push("/sign-in");
+//           } catch {
+//             // noop
+//           }
+//         }}
+//       >
+//         Sign out
+//       </Button>
+//     );
+//   }
+//   return <div>Not signed in</div>;
+// }
