eca0c57dc2c950470bf03a3b001d970b1bec59c1
Caleb Rivera <calebrossr@gmail.com>
2024-03-06 00:01:22 -0500

    Cook

--- apps/expo/app/index.tsx
+++ apps/expo/app/index.tsx
@@ -1,20 +1,11 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text className="bg-red-500">
+    <View>
+      <Text className="bg-blue-500">
         Open up App.js to start working on your app!
       </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
