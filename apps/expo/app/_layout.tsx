4765af6b6ddf9e36a6d146ee1b0829c8b1de101f
Caleb Rivera <calebrossr@gmail.com>
2024-03-05 23:32:03 -0500

    Copy working expo example

---
+++ apps/expo/app/_layout.tsx
@@ -0,0 +1,6 @@
+import { Slot } from "expo-router";
+
+// Import your global CSS file
+import "../globals.css";
+
+export default Slot;
---
+++ apps/expo/app/index.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text className="bg-red-500">
+        Open up App.js to start working on your app!
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
