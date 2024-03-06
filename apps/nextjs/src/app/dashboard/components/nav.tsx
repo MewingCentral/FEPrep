7a2d3bdca6099e6b145250db4209be9674aaa914
Pharit Smitasin <pharitsmitasin@gmail.com>
2024-03-05 23:11:38 -0500

    add underline to center nav links

--- apps/nextjs/src/app/dashboard/components/nav.tsx
+++ apps/nextjs/src/app/dashboard/components/nav.tsx
@@ -19,7 +19,7 @@ export function Nav() {
         <Image src="/Ellipse-3.svg" width={25} height={25} alt="FEPrep Logo" />
         <Link
           href="/"
-          className="link link-underline link-underline-black text-left text-xl font-semibold transition-all duration-200 ease-in-out"
+          className="link link-underline link-underline-black text-left text-xl font-semibold"
         >
           FEPrep
         </Link>
@@ -40,14 +40,18 @@ export function Nav() {
       </div>
       <div className="flex grow flex-row justify-center gap-3">
         <LapTimerIcon width="25" height="25" />
-        <h1 className="text-slate text-left text-xl font-normal ">Timer</h1>
+        <h1 className="text-slate link link-underline link-underline-black text-left text-xl font-normal">
+          Timer
+        </h1>
         <Separator
           orientation="vertical"
           decorative={true}
           className="w-0.5 bg-foreground"
         />
         <RocketIcon width="25" height="25" className="" />
-        <h1 className="text-slate text-left text-xl font-medium ">Solution</h1>
+        <h1 className="text-slate link link-underline link-underline-black text-left text-xl font-medium ">
+          Solution
+        </h1>
       </div>
       <div className="flex flex-row-reverse items-end gap-3">
         <h1 className="text-slate text-left text-xl font-medium ">NID</h1>
