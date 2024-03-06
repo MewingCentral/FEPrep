5e7e87bb2c4a228d1b31b6b327753690fda3893e
Pharit Smitasin <pharitsmitasin@gmail.com>
2024-03-05 23:28:51 -0500

    add topics component, to be styled

---
+++ apps/nextjs/src/app/dashboard/components/topics.tsx
@@ -0,0 +1,57 @@
+export default function topics() {
+  return (
+    <>
+      <div className="hover:bg- transform cursor-pointer bg-border duration-500 hover:bg-muted-foreground">
+        ALGORITHM ANALYSIS
+      </div>
+      <div className="transform cursor-pointer bg-border duration-500 hover:bg-muted-foreground">
+        AVL TREES
+      </div>
+      <div className="transform cursor-pointer bg-border duration-500 hover:bg-muted-foreground">
+        BACKTRACKING
+      </div>
+      <div className="transform cursor-pointer bg-border duration-500 hover:bg-muted-foreground">
+        BASE CONVERSION
+      </div>
+      <div className="transform cursor-pointer bg-border duration-500 hover:bg-muted-foreground">
+        BINARY TREES
+      </div>
+      <div className="transform cursor-pointer bg-border duration-500 hover:bg-muted-foreground">
+        BITWISE OPERATORS
+      </div>
+      <div className="transform cursor-pointer bg-border duration-500 hover:bg-muted-foreground">
+        DYNAMIC MEMORY MANAGEMENT IN C
+      </div>
+      <div className="transform cursor-pointer bg-border duration-500 hover:bg-muted-foreground">
+        HASH TABLES
+      </div>
+      <div className="transform cursor-pointer bg-border duration-500 hover:bg-muted-foreground">
+        HEAPS
+      </div>
+      <div className="transform cursor-pointer bg-border duration-500 hover:bg-muted-foreground">
+        LINKED LISTS
+      </div>
+      <div className="transform cursor-pointer bg-border duration-500 hover:bg-muted-foreground">
+        QUEUES
+      </div>
+      <div className="transform cursor-pointer bg-border duration-500 hover:bg-muted-foreground">
+        RECURRENCE RELATIONS
+      </div>
+      <div className="transform cursor-pointer bg-border duration-500 hover:bg-muted-foreground">
+        RECURSIVE CODING
+      </div>
+      <div className="transform cursor-pointer bg-border duration-500 hover:bg-muted-foreground">
+        SORTING
+      </div>
+      <div className="transform cursor-pointer bg-border duration-500 hover:bg-muted-foreground">
+        STACKS
+      </div>
+      <div className="transform cursor-pointer bg-border duration-500 hover:bg-muted-foreground">
+        SUMMATIONS
+      </div>
+      <div className="transform cursor-pointer bg-border duration-500 hover:bg-muted-foreground">
+        TRIES
+      </div>
+    </>
+  );
+}
