379506c5f06a5b8530b06c20039faef53aa16e36
anna-sage-local <anna.macinnis109@gmail.com>
2024-03-10 15:52:18 -0400

    initial tab styling

--- apps/expo/src/app/dashboard/(tabs)/_layout.tsx
+++ apps/expo/src/app/dashboard/(tabs)/_layout.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 import { Tabs } from "expo-router";
 
+import Colors from "../../../utils/colors";
+
 export default function TabLayout() {
     return (
-        <Tabs screenOptions={{}}>
+        <Tabs screenOptions={{
+            tabBarActiveBackgroundColor: Colors.dark_sec,
+            tabBarInactiveBackgroundColor: Colors.dark_sec,
+            tabBarActiveTintColor: Colors.dark_primary_text,
+            tabBarInactiveTintColor: Colors.dark_secondary_text,
+
+            tabBarLabelPosition: "beside-icon",
+            tabBarLabelStyle: {
+                fontSize: 16,
+            },
+            tabBarIconStyle: {
+                display: "none",
+            }
+        }}>
             <Tabs.Screen name="studySets" options={{
                 title: "Study Sets",
             }}/>
