cf58a02166ccd6f7e9a19ec66dd8eb40945fbd2a
anna-sage-local <anna.macinnis109@gmail.com>
2024-03-10 18:18:07 -0400

    dashboard initial design complete

--- apps/expo/src/app/dashboard/(tabs)/studySets.tsx
+++ apps/expo/src/app/dashboard/(tabs)/studySets.tsx
@@ -1,9 +1,86 @@
-import { View, Text } from "react-native";
+import { StyleSheet, View, Text, TextInput, Pressable } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { Stack } from "expo-router";
+
+import Colors from "../../../utils/colors";
 
 export default function Tab() {
     return (
-        <View>
-            <Text>Tab [Dashboard | Study Sets]</Text>
-        </View>
+        <SafeAreaView style={[styles.screenContainer, styles.container]}>
+            {/* Search input */}
+            <View style={{ flexDirection: "row" }}>
+                <View style={[styles.container, styles.inputContainer]}>
+                    <TextInput style={[styles.input]} placeholder={"Enter set"} placeholderTextColor={Colors.dark_secondary_text} cursorColor={Colors.dark_primary_text}/>
+                </View>
+            </View>
+
+            {/* Study sets */}
+            <View style={[styles.container, styles.allSetsContainer]}>
+                {/* Individual study set */}
+                <View style={[styles.container, styles.setContainer]}>
+                    <View  style={[styles.setTextContainer]}>
+                        <Text style={[styles.setText, styles.titleText]}>Set 1</Text>
+                        <Text style={[styles.setText, styles.setTerms]}>15 terms</Text>
+                    </View>
+                </View>
+            </View>
+
+        </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: "column",
+        flexWrap: "wrap",
+        backgroundColor: Colors.dark_bg,
+    },
+    screenContainer: {
+        flexGrow: 1,
+        gap: 30,
+        alignContent: "center",
+        alignItems: "center",
+        backgroundColor: Colors.dark_bg,
+    },
+    inputContainer: {
+        flex: 1,
+        alignContent: "stretch",
+        marginHorizontal: 20,
+    },
+    allSetsContainer: {
+        gap: 20,
+    },
+    setContainer: {
+        height: 100,
+        width: 300,
+        backgroundColor: Colors.dark_sec,
+        borderColor: "rgba(148, 163, 184, 0.50)",
+        borderWidth: 2,
+        borderRadius: 6,
+    },
+    setTextContainer: {
+        justifyContent: "space-between",
+        height: 100,
+    },
+    input: {
+        color: Colors.dark_primary_text,
+        borderBottomWidth: 1,
+        borderBottomColor: Colors.dark_primary_text,
+    },
+    titleText: {
+        color: Colors.dark_primary_text,
+        fontSize: 20,
+    },
+    setText: {
+        margin: 10,
+    },
+    setTerms: {
+        color: Colors.dark_secondary_text,
+        fontSize: 16,
+    },
+    deleteYellowBorder: {
+        borderColor: "yellow",
+        borderWidth: 1,
+    }
+});
