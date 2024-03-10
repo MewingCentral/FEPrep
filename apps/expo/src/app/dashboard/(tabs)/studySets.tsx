7726e444021bf6f7896837fefd125f5585d7ffde
anna-sage-local <anna.macinnis109@gmail.com>
2024-03-10 14:38:03 -0400

    dashboard tabs working

---
+++ apps/expo/src/app/dashboard/(tabs)/studySets.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Tab() {
+    return (
+        <View>
+            <Text>Tab [Dashboard | Study Sets]</Text>
+        </View>
+    );
+}
