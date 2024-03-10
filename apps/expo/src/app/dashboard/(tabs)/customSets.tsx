72b247a319708dda3c7d9378c173882d081fbfa9
anna-sage-local <anna.macinnis109@gmail.com>
2024-03-10 17:09:55 -0400

    dashboard progress input alignment

--- apps/expo/src/app/dashboard/(tabs)/customSets.tsx
+++ apps/expo/src/app/dashboard/(tabs)/customSets.tsx
@@ -10,15 +10,18 @@ export default function Tab() {
         <SafeAreaView style={[styles.deleteYellowBorder, styles.container, styles.screenContainer]}>
 
             {/* Search input */}
-            <View style={[styles.deleteYellowBorder, styles.container, styles.inputContainer]}>
-                <TextInput style={[styles.input]} placeholder={"Enter set"} placeholderTextColor={Colors.dark_primary_text} cursorColor={Colors.dark_primary_text}/>
+            <View style={{ flexDirection: "row" }}>
+                <View style={[styles.deleteYellowBorder, styles.container, styles.inputContainer]}>
+                    <TextInput style={[styles.input]} placeholder={"Enter set"} placeholderTextColor={Colors.dark_secondary_text} cursorColor={Colors.dark_primary_text}/>
+                </View>
             </View>
 
             {/* Study sets */}
-            <View style={[styles.deleteYellowBorder, styles.container]}>
+            <View style={[styles.container]}>
                 {/* Individual study set */}
-                <View style={[styles.deleteYellowBorder, styles.container]}>
-
+                <View style={[styles.container, styles.setContainer]}>
+                    <Text style={[styles.setTitle]}>Set 1</Text>
+                    <Text style={[]}>15 terms</Text>
                 </View>
             </View>
         </SafeAreaView>
@@ -29,24 +32,41 @@ const styles = StyleSheet.create({
     container: {
         flexDirection: "column",
         flexWrap: "wrap",
-        alignItems: "center",
         backgroundColor: Colors.dark_bg,
     },
     screenContainer: {
-        flex: 1,
+        flexGrow: 1,
         gap: 30,
-        alignContent: "center", // Horizontal alignment of children
-        justifyContent: "center",
+        alignContent: "center",
+        alignItems: "center",
         backgroundColor: Colors.dark_bg,
     },
     inputContainer: {
-        
+        flex: 1,
+        alignContent: "stretch",
+        marginHorizontal: 10,
+        // alignSelf: "stretch",
+    },
+    allSetsContainer: {
+    },
+    setContainer: {
+        height: 100,
+        width: 300,
+        backgroundColor: Colors.dark_sec,
+        borderColor: "rgba(148, 163, 184, 0.33)",
+        borderWidth: 2,
+        borderRadius: 6,
     },
     input: {
         color: Colors.dark_primary_text,
         borderBottomWidth: 1,
         borderBottomColor: Colors.dark_primary_text,
         marginBottom:4, // delete
+    },
+    setTitle: {
+        margin: 10,
+        color: Colors.dark_primary_text,
+        fontSize: 20,
     },
     deleteYellowBorder: {
         borderColor: "yellow",
