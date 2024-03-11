2f0b78e90fdbab2b0a1fb00fefbdef76e0cb6379
Luis Infante <113048160+LuisMInfante@users.noreply.github.com>
2024-03-11 00:38:22 -0400

    fixed formatting

--- apps/nextjs/src/app/dashboard/components/left.tsx
+++ apps/nextjs/src/app/dashboard/components/left.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 import {
   Card,
@@ -7,24 +7,23 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@feprep/ui/card"
+} from "@feprep/ui/card";
 
 export function Left() {
   return (
-    <Card className="flex flex-col h-full">
+    <Card className="flex h-full flex-col">
       <CardHeader>
         {/* This will need to be dynamic based on question */}
         <CardTitle>Section A: Basic Data Structures | Spring 2024</CardTitle>
         <CardDescription>10 Points</CardDescription>
       </CardHeader>
       <CardContent className="flex-1">
-        <div className="h-full bg-gray-600"> {/* Just a placeholder background for visibility */}
+        <div className="h-full bg-gray-600">
+          {/* Just a placeholder background for visibility */}
           {/* Need the Markdown here */}
         </div>
       </CardContent>
-      <CardFooter>
-        {/* Need to add something here */}
-      </CardFooter>
+      <CardFooter>{/* Need to add something here */}</CardFooter>
     </Card>
   );
 }
