452746c7f8d05cc41e6bb6782894c2f1233085a6
qnbby <quan.nguyen.duc.3010@gmail.com>
2024-03-11 17:06:14 -0400

    Add right component into dashboard

--- apps/nextjs/src/app/dashboard/dashboard.tsx
+++ apps/nextjs/src/app/dashboard/dashboard.tsx
@@ -7,34 +7,23 @@
 
 // import { api } from "~/trpc/react";
 import { Nav } from "./components/nav";
+import { Right } from "./components/right"; // for when this is implemented
 
 export function Dashboard() {
   return (
     <main>
       <Nav />
-    </main>
-  )
+      {/* <AuthButton user={user} /> */}
+      <main className="flex flex-1">
+        <div className="h-full w-1/2">
+          <Left />
+        </div>
+
+        <div className="h-full w-1/2 bg-gray-200">
+          {/* Just a placeholder background for visibility */}
+          {/* <Right /> */}
+        </div>
+      </main>
+    </div>
+  );
 }
-
-// function AuthButton({ user }: { user: User | null }) {
-//   const { mutateAsync } = api.auth.signOut.useMutation();
-//   const router = useRouter();
-
-//   if (user) {
-//     return (
-//       <Button
-//         onClick={async () => {
-//           try {
-//             await mutateAsync();
-//             router.push("/sign-in");
-//           } catch {
-//             // noop
-//           }
-//         }}
-//       >
-//         Sign out
-//       </Button>
-//     );
-//   }
-//   return <div>Not signed in</div>;
-// }
