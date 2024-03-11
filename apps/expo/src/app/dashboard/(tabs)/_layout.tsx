682eb669de5d1b0d363c2a58d99a6728c3db0eba
anna-sage-local <anna.macinnis109@gmail.com>
2024-03-11 11:12:02 -0400

    more import refactoring

--- apps/expo/src/app/dashboard/(tabs)/_layout.tsx
+++ apps/expo/src/app/dashboard/(tabs)/_layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Tabs } from "expo-router";
 
-import Colors from "../../../utils/colors";
+import Colors from "~/utils/colors";
 
 export default function TabLayout() {
   return (
