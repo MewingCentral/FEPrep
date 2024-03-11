de20abd7e0a275acf0ea0389904da996d41c057c
Pharit Smitasin <pharitsmitasin@gmail.com>
2024-03-11 17:33:37 -0400

    add separators to nav

--- apps/nextjs/src/app/dashboard/components/nav.tsx
+++ apps/nextjs/src/app/dashboard/components/nav.tsx
@@ -8,8 +8,7 @@ import {
   RocketIcon,
   ShuffleIcon,
 } from "@feprep/ui";
-
-// import { Separator } from "@feprep/ui/separator";
+import { Separator } from "@feprep/ui/separator";
 
 export function Nav() {
   return (
@@ -18,7 +17,11 @@ export function Nav() {
       <div className="flex flex-row gap-3">
         <Image src="/Ellipse-3.svg" width={25} height={25} alt="FEPrep Logo" />
         <h1 className="text-left text-xl font-semibold">FEPrep</h1>
-        {/* add div line here */}
+        <Separator
+          orientation="vertical"
+          decorative={true}
+          className="w-0.5 bg-foreground"
+        />
         <h1 className="text-slate text-left text-xl font-normal  underline">
           Topics
         </h1>
@@ -29,6 +32,11 @@ export function Nav() {
       <div className="flex grow flex-row justify-center gap-2">
         <LapTimerIcon width="25" height="25" />
         <h1 className="text-slate text-left text-xl font-normal ">Timer</h1>
+        <Separator
+          orientation="vertical"
+          decorative={true}
+          className="w-0.5 bg-foreground"
+        />
         <RocketIcon width="25" height="25" className="" />
         <h1 className="text-slate text-left text-xl font-medium ">Solution</h1>
       </div>
