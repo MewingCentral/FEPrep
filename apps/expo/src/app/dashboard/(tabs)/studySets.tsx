77a6ee83d381944af760875e757c31377725fbce
anna-sage-local <anna.macinnis109@gmail.com>
2024-03-11 11:08:41 -0400

    moved dashboard styles to new file

--- apps/expo/src/app/dashboard/(tabs)/studySets.tsx
+++ apps/expo/src/app/dashboard/(tabs)/studySets.tsx
@@ -1,16 +1,17 @@
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { Text, TextInput, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import Colors from "~/utils/colors";
+import dashStyles from "~/utils/dashStyles";
 
 export default function Tab() {
   return (
-    <SafeAreaView style={[styles.screenContainer, styles.container]}>
+    <SafeAreaView style={[dashStyles.screenContainer, dashStyles.container]}>
       {/* Search input */}
       <View style={{ flexDirection: "row" }}>
-        <View style={[styles.container, styles.inputContainer]}>
+        <View style={[dashStyles.container, dashStyles.inputContainer]}>
           <TextInput
-            style={[styles.input]}
+            style={[dashStyles.input]}
             placeholder={"Enter set"}
             placeholderTextColor={Colors.dark_secondary_text}
             cursorColor={Colors.dark_primary_text}
@@ -19,66 +20,19 @@ export default function Tab() {
       </View>
 
       {/* Study sets */}
-      <View style={[styles.container, styles.allSetsContainer]}>
+      <View style={[dashStyles.container, dashStyles.allSetsContainer]}>
         {/* Individual study set */}
-        <View style={[styles.container, styles.setContainer]}>
-          <View style={[styles.setTextContainer]}>
-            <Text style={[styles.setText, styles.titleText]}>Set 1</Text>
-            <Text style={[styles.setText, styles.setTerms]}>15 terms</Text>
+        <View style={[dashStyles.container, dashStyles.setContainer]}>
+          <View style={[dashStyles.setTextContainer]}>
+            <Text style={[dashStyles.setText, dashStyles.titleText]}>
+              Set 1
+            </Text>
+            <Text style={[dashStyles.setText, dashStyles.setTerms]}>
+              15 terms
+            </Text>
           </View>
         </View>
       </View>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: "column",
-    flexWrap: "wrap",
-    backgroundColor: Colors.dark_bg,
-  },
-  screenContainer: {
-    flexGrow: 1,
-    gap: 30,
-    alignContent: "center",
-    alignItems: "center",
-    backgroundColor: Colors.dark_bg,
-  },
-  inputContainer: {
-    flex: 1,
-    alignContent: "stretch",
-    marginHorizontal: 20,
-  },
-  allSetsContainer: {
-    gap: 20,
-  },
-  setContainer: {
-    height: 100,
-    width: 300,
-    backgroundColor: Colors.dark_sec,
-    borderColor: "rgba(148, 163, 184, 0.50)",
-    borderWidth: 2,
-    borderRadius: 6,
-  },
-  setTextContainer: {
-    justifyContent: "space-between",
-    height: 100,
-  },
-  input: {
-    color: Colors.dark_primary_text,
-    borderBottomWidth: 1,
-    borderBottomColor: Colors.dark_primary_text,
-  },
-  titleText: {
-    color: Colors.dark_primary_text,
-    fontSize: 20,
-  },
-  setText: {
-    margin: 10,
-  },
-  setTerms: {
-    color: Colors.dark_secondary_text,
-    fontSize: 16,
-  },
-});
