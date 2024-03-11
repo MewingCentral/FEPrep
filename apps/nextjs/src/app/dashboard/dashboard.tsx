5c0db50cdc6199cde8c557dccf2db7ad163d8d6c
Luis Infante <113048160+LuisMInfante@users.noreply.github.com>
2024-03-11 00:00:45 -0400

    initial commit

--- apps/nextjs/src/app/dashboard/dashboard.tsx
+++ apps/nextjs/src/app/dashboard/dashboard.tsx
@@ -2,11 +2,14 @@
 
 // import { useRouter } from "next/navigation";
 // import { User } from "@feprep/auth";
+import { useRouter } from "next/navigation";
 
 // import { Button } from "@feprep/ui/button";
 
 // import { api } from "~/trpc/react";
 import { Nav } from "./components/nav";
+import { Left } from "./components/left";
+// import { Right } from "./components/right"; // for when this is implemented
 
 // export function Dashboard({ user }: { user: User | null }) {
 //   return (
@@ -19,10 +22,20 @@ import { Nav } from "./components/nav";
 
 export function Dashboard() {
   return (
-    <main>
+    <div className="flex flex-col h-screen">
       <Nav />
+    
       {/* <AuthButton user={user} /> */}
-    </main>
+      <main className="flex flex-1">
+        <div className="w-1/2 h-full">
+          <Left />
+        </div>
+      
+        <div className="w-1/2 h-full bg-gray-200"> {/* Just a placeholder background for visibility */}
+          {/* <Right /> */}
+        </div>
+      </main>
+    </div>
   );
 }
 
