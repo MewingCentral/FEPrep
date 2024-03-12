56ac5bddaf3f6b6b66f96a9f1b0ed364a0ccf0d2
anna-sage-local <anna.macinnis109@gmail.com>
2024-03-12 19:51:38 -0400

    added default header nav styling utility

--- apps/expo/src/utils/headerDefault.tsx
+++ apps/expo/src/utils/headerDefault.tsx
@@ -1,17 +1,37 @@
-import Colors from "colors";
+import Colors from "./colors";
+import { useColorScheme } from "nativewind";
 
-export default function headerDefault(colorScheme) {
-    return ({
-        headerStyle: {
-            backgroundColor:
-              colorScheme == "dark" ? Colors.dark_sec : Colors.light_sec,
-          },
-          contentStyle: {
-            backgroundColor: colorScheme == "dark" ? "#020817" : "#F8FAFC",
-          },
-          headerTintColor:
-            colorScheme == "dark"
-              ? Colors.dark_primary_text
-              : Colors.light_primary_text,
-    });
-}
+const { colorScheme } = useColorScheme();
+
+const headerDefault = {
+    headerStyle: {
+        backgroundColor:
+            colorScheme == "dark" ? Colors.dark_sec : Colors.light_sec,
+    },
+    contentStyle: {
+        backgroundColor: colorScheme == "dark" ? "#020817" : "#F8FAFC",
+    },
+        headerTintColor:
+        colorScheme == "dark"
+            ? Colors.dark_primary_text
+            : Colors.light_primary_text,
+};
+
+export default headerDefault;
+
+
+// export default function headerDefault(colorScheme) {
+//     return ({
+//         headerStyle: {
+//             backgroundColor:
+//               colorScheme == "dark" ? Colors.dark_sec : Colors.light_sec,
+//           },
+//           contentStyle: {
+//             backgroundColor: colorScheme == "dark" ? "#020817" : "#F8FAFC",
+//           },
+//           headerTintColor:
+//             colorScheme == "dark"
+//               ? Colors.dark_primary_text
+//               : Colors.light_primary_text,
+//     });
+// }
