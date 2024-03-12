4fe5d3856aec837a54f61de5c1c451dea98bedd2
anna-sage-local <anna.macinnis109@gmail.com>
2024-03-12 19:51:38 -0400

    added default header nav styling utility

--- apps/expo/src/app/cardCreation/_layout.tsx
+++ apps/expo/src/app/cardCreation/_layout.tsx
@@ -4,24 +4,12 @@ import { Stack } from "expo-router";
 import { useColorScheme } from "nativewind";
 
 import Colors from "~/utils/colors";
+import headerDefault from "~/utils/headerDefault";
 
 export default function cardCreationLayout() {
     const { colorScheme } = useColorScheme();
     return (
-        <Stack screenOptions={{
-            // headerShown: false,
-            title: "Custom set: Untitled",
-            headerStyle: {
-                backgroundColor: Colors.dark_sec,
-            },
-            contentStyle: {
-                backgroundColor: Colors.dark_bg,
-            },
-            headerTintColor:
-            colorScheme == "dark"
-              ? Colors.dark_primary_text
-              : Colors.light_primary_text,
-        }}>
+        <Stack screenOptions={headerDefault}>
         </Stack>
     );
 };
