5222996d616f7f7ed45d20eb8caedc206bbf5b6a
Caleb Rivera <calebrossr@gmail.com>
2024-03-13 09:03:29 -0400

    Create nav

---
+++ apps/nextjs/src/app/explore/nav.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { Separator } from "@feprep/ui/separator";
+
+export function Nav() {
+  return (
+    <nav className="flex h-16 items-center justify-between px-6">
+      <div className="mr-5 flex h-8 items-center gap-3">
+        <Image src="/Ellipse-3.svg" width={25} height={25} alt="FEPrep Logo" />
+        <Link
+          href="/"
+          className="hidden text-left text-xl font-semibold hover:underline md:block"
+        >
+          FEPrep
+        </Link>
+        <Separator orientation="vertical" decorative />
+        <Link href="/dashboard">Dashboard</Link>
+      </div>
+
+      <div>Not Signed In</div>
+    </nav>
+  );
+}
