047e3b86600a807e8951a7b22fd8347c55e5bfd8
Pharit Smitasin <pharitsmitasin@gmail.com>
2024-03-13 19:10:49 -0400

    remove unecessary comments from dashboard for readability

--- apps/nextjs/src/app/dashboard/dashboard.tsx
+++ apps/nextjs/src/app/dashboard/dashboard.tsx
@@ -8,21 +8,17 @@ import { Left } from "./components/left";
 import { Nav } from "./components/nav";
 import { Right } from "./components/right";
 
-// import { Right } from "./components/right"; // for when this is implemented
-
 export function Dashboard({ user }: { user: User | null }) {
   return (
     <div className="flex h-screen flex-col">
       <Nav user={user} />
 
-      {/* <AuthButton user={user} /> */}
       <main className="flex flex-1 gap-4 p-4">
         <div className="h-full flex-1">
           <Left />
         </div>
 
         <div className="h-full flex-1">
-          {/* Just a placeholder background for visibility */}
           <Right />
         </div>
       </main>
