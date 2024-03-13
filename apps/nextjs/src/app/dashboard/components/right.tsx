eea26f11b75e6cf3250402435bbe2999b1f204a9
Caleb Rivera <calebrossr@gmail.com>
2024-03-12 22:06:35 -0400

    I'm gonna kiss Quan on the forehead pt.2 (with consent)

--- apps/nextjs/src/app/dashboard/components/right.tsx
+++ apps/nextjs/src/app/dashboard/components/right.tsx
@@ -2,31 +2,27 @@ import { DotsHorizontalIcon, FileIcon, VideoIcon } from "@feprep/ui";
 
 export function Right() {
   return (
-    <main>
-      <div className="flex h-full flex-col rounded-xl">
-        {/* top right */}
-        <div className="top-section justify-left flex w-full grow flex-row gap-2 rounded-xl border p-4">
-          <DotsHorizontalIcon width="25" height="25" />
-          <h1 className="text-slate font-small mr-2 text-left text-xl">
-            Discussion
-          </h1>
-          <FileIcon width="25" height="25" />
-          <h1 className="text-slate font-small mr-2 text-left text-xl">
-            Notes
-          </h1>
-          <VideoIcon width="25" height="25" />
-          <h1 className="text-slate font-small text-left text-xl">Resources</h1>
-        </div>
-        {/* bottom right */}
-        <div className="bottom-section mt-4 w-full rounded-xl border">
-          <h1 className="text-slate font-small px-4 py-2 text-left text-xl">
-            comment
-          </h1>
-          <h1 className="text-slate font-small px-4 py-2 text-left text-xl">
-            padding
-          </h1>
-        </div>
+    <div className="flex h-full flex-col rounded-xl">
+      {/* top right */}
+      <div className="top-section justify-left flex w-full flex-row gap-2 rounded-xl border p-4">
+        <DotsHorizontalIcon width="25" height="25" />
+        <h1 className="text-slate font-small mr-2 text-left text-xl">
+          Discussion
+        </h1>
+        <FileIcon width="25" height="25" />
+        <h1 className="text-slate font-small mr-2 text-left text-xl">Notes</h1>
+        <VideoIcon width="25" height="25" />
+        <h1 className="text-slate font-small text-left text-xl">Resources</h1>
       </div>
-    </main>
+      {/* bottom right */}
+      <div className="bottom-section mt-4 w-full flex-1 rounded-xl border">
+        <h1 className="text-slate font-small px-4 py-2 text-left text-xl">
+          comment
+        </h1>
+        <h1 className="text-slate font-small px-4 py-2 text-left text-xl">
+          padding
+        </h1>
+      </div>
+    </div>
   );
 }
