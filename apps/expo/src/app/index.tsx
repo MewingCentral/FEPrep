fa31f74d58b81716a6317a540883d24e7d3ffb75
anna-sage-local <anna.macinnis109@gmail.com>
2024-03-13 13:00:29 -0400

    replaced erroneous homepage and deleted deprecated dashboard

--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -1,39 +1,111 @@
-import { Pressable, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Link, Stack } from "expo-router";
 
 export default function HomePage() {
   return (
-    <SafeAreaView className="bg-background">
-      <Stack.Screen />
-      <View>
-        <Text className="text-center text-4xl font-bold text-primary">
-          FEPrep
-        </Text>
-        <Text className="text-center text-2xl font-bold text-primary">
-          A new way to prepare for the Foundation Exam.
-        </Text>
-        <View className="mt-4 flex flex-col gap-6">
-          <View className="h-16 bg-foreground">
-            <Link href="/dashboard" asChild className="my-auto">
-              <Pressable>
-                <Text className="text-center text-primary">
-                  Start Practicing
-                </Text>
-              </Pressable>
-            </Link>
-          </View>
-          <View className="h-16 bg-foreground">
-            <Link href="/dashboard" asChild className="my-auto">
-              <Pressable>
-                <Text className="text-center text-primary">
-                  Create an Account
-                </Text>
-              </Pressable>
-            </Link>
-          </View>
+    <SafeAreaView style={[styles.container, styles.screenContainer]}>
+      <Stack.Screen
+        options={{
+          headerShown: false,
+        }}
+      />
+
+      {/* Title and subtitle */}
+      <View style={[styles.container, styles.contentContainer]}>
+        <View style={[styles.container, styles.titleContainer]}>
+          <Text style={[styles.text, styles.title]}>FEPrep</Text>
+          <Text style={[styles.text, styles.subtitle]}>
+            A new way to prepare for the Foundation Exam.
+          </Text>
+        </View>
+
+        {/* Buttons */}
+        <View style={[styles.container, styles.buttonsContainer]}>
+          <Link
+            style={[styles.buttons, styles.signUpBtn]}
+            href="/dashboard/(tabs)/studySets"
+            asChild
+          >
+            <Pressable>
+              <Text style={[styles.text, styles.buttonTxt]}>
+                Create Account
+              </Text>
+            </Pressable>
+          </Link>
+          <Text style={[styles.text]}>or</Text>
+          <Link
+            style={[styles.buttons, styles.dashboardBtn]}
+            href="/dashboard/(tabs)/studySets"
+            asChild
+          >
+            <Pressable>
+              <Text style={[styles.text, styles.buttonTxt]}>
+                Start Practicing
+              </Text>
+            </Pressable>
+          </Link>
         </View>
       </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "column",
+    flexWrap: "wrap",
+  },
+  screenContainer: {
+    flex: 1,
+    alignContent: "center", // Horizontal alignment of children
+    justifyContent: "center", // Vertical alignment of children
+  },
+  contentContainer: {
+    alignSelf: "center",
+    gap: 50,
+    maxWidth: 305,
+    borderRadius: 6,
+    backgroundColor: "#f8fafa",
+  },
+  titleContainer: {
+    marginTop: 40,
+    marginHorizontal: 20,
+    flexWrap: "nowrap",
+    gap: 10,
+  },
+  buttonsContainer: {
+    alignSelf: "center",
+    marginHorizontal: 20,
+    marginBottom: 40,
+    gap: 10,
+  },
+  text: {
+    color: "#020817",
+    textAlign: "center",
+    fontSize: 20,
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    lineHeight: 30,
+    fontSize: 20,
+  },
+  buttons: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    minWidth: 70,
+    borderRadius: 6,
+  },
+  signUpBtn: {
+    backgroundColor: "#64748b",
+  },
+  dashboardBtn: {
+    backgroundColor: "#0f172a",
+  },
+  buttonTxt: {
+    color: "#f8fafc",
+  },
+});
