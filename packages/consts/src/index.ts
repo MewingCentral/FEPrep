10f1beb123c3cba939cd7c58c67d167c58432921
Caleb Rivera <calebrossr@gmail.com>
2024-03-14 22:44:00 -0400

    Create consts package

---
+++ packages/consts/src/index.ts
@@ -0,0 +1,32 @@
+export const difficulties = ["Easy", "Medium", "Hard"] as const;
+
+export const semesters = [
+  "Fall 2024",
+  "Summer 2024",
+  "Spring 2024",
+  "Fall 2023",
+  "Summer 2023",
+  "Spring 2023",
+  "Fall 2022",
+  "Summer 2022",
+  "Spring 2022",
+] as const;
+
+export const topics = [
+  "Algorithm Analysis",
+  "AVL Trees",
+  "Base Conversion",
+  "Binary Trees",
+  "Bitwise Operators",
+  "Dynamic Memory Management",
+  "Hash Tables",
+  "Heaps",
+  "Linked Lists",
+  "Queues",
+  "Recurrence Relations",
+  "Recursive Coding",
+  "Sorting",
+  "Stacks",
+  "Summations",
+  "Tries",
+] as const;
