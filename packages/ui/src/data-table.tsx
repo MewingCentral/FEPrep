7f2ffe50a25c0e0114599b38700d9486d595f589
Caleb Rivera <calebrossr@gmail.com>
2024-03-14 23:43:28 -0400

    Export data table component

--- packages/ui/src/data-table.tsx
+++ packages/ui/src/data-table.tsx
@@ -1,4 +1,8 @@
-import type { Column, Table } from "@tanstack/react-table";
+import type {
+  Column,
+  ColumnDef,
+  Table as ReactTable,
+} from "@tanstack/react-table";
 import {
   DropdownMenu,
   DropdownMenuCheckboxItem,
@@ -15,6 +19,11 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@radix-ui/react-select";
+import {
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
 
 import {
   ArrowDownIcon,
@@ -29,6 +38,14 @@ import {
   MixerHorizontalIcon,
 } from ".";
 import { Button } from "./button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./table";
 
 interface DataTableColumnHeaderProps<TData, TValue>
   extends React.HTMLAttributes<HTMLDivElement> {
@@ -85,7 +102,7 @@ export function DataTableColumnHeader<TData, TValue>({
 }
 
 interface DataTablePaginationProps<TData> {
-  table: Table<TData>;
+  table: ReactTable<TData>;
 }
 
 export function DataTablePagination<TData>({
@@ -166,7 +183,7 @@ export function DataTablePagination<TData>({
 }
 
 interface DataTableViewOptionsProps<TData> {
-  table: Table<TData>;
+  table: ReactTable<TData>;
 }
 
 export function DataTableViewOptions<TData>({
@@ -207,5 +224,68 @@ export function DataTableViewOptions<TData>({
           })}
       </DropdownMenuContent>
     </DropdownMenu>
+  );
+}
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <div className="rounded-md border">
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext(),
+                        )}
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
   );
 }
