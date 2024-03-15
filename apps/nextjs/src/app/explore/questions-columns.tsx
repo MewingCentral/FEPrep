76d5f00a0ede31cf0c8f6386848f85ad375ef667
Caleb Rivera <calebrossr@gmail.com>
2024-03-15 00:43:13 -0400

    Update questions table columns

--- apps/nextjs/src/app/explore/questions-columns.tsx
+++ apps/nextjs/src/app/explore/questions-columns.tsx
@@ -6,7 +6,39 @@ export const questionsColumns: ColumnDef<
   RouterOutputs["questions"]["all"][number]
 >[] = [
   {
-    accessorKey: "id",
-    header: "ID",
+    accessorKey: "Title",
+    accessorFn: (row) =>
+      `${row.semester} ${row.section} Question ${row.questionNumber}`,
+  },
+  {
+    accessorKey: "Topic",
+    accessorFn: (row) => row.topic,
+  },
+  {
+    accessorKey: "Difficulty",
+    accessorFn: (row) => {
+      if (row.easyVotes > row.mediumVotes && row.easyVotes > row.hardVotes) {
+        return "Easy";
+      } else if (
+        row.mediumVotes > row.easyVotes &&
+        row.mediumVotes > row.hardVotes
+      ) {
+        return "Medium";
+      } else {
+        return "Hard";
+      }
+    },
+  },
+  {
+    accessorKey: "Points",
+    accessorFn: (row) => row.points,
+  },
+  {
+    accessorKey: "Average Score",
+    accessorFn: (row) => row.averageScore,
+  },
+  {
+    accessorKey: "Votes",
+    accessorFn: (row) => row.easyVotes + row.mediumVotes + row.hardVotes,
   },
 ];
