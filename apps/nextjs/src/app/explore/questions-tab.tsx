78ac982d7678689ed9587f34e3cfeb88d30ea58f
Caleb Rivera <calebrossr@gmail.com>
2024-03-16 06:01:40 -0400

    Add loading skeleton when loading data

--- apps/nextjs/src/app/explore/questions-tab.tsx
+++ apps/nextjs/src/app/explore/questions-tab.tsx
@@ -1,11 +1,18 @@
 "use client";
 
+import { Skeleton } from "@feprep/ui/skeleton";
+
 import { api } from "~/trpc/react";
 import { questionsColumns } from "./questions-columns";
 import { QuestionsTable } from "./questions-table";
 
 export function QuestionsTab() {
   const getAllQuestions = api.questions.all.useQuery();
+
+  // TODO: make this a lot nicer
+  if (getAllQuestions.isLoading) {
+    return <Skeleton className="h-40 w-full" />;
+  }
 
   return (
     <QuestionsTable
