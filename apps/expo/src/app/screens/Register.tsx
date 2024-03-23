0c1447fe7a98ff487cb738ad0e898d4c13fb1129
e4s70f3d3n <annalisa.vitulli@ucf.edu>
2024-03-23 16:46:58 -0400

    updatelogin page for new navigation and add registration page

--- apps/expo/src/app/screens/Register.tsx
+++ apps/expo/src/app/screens/Register.tsx
@@ -14,7 +14,7 @@ import {
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
-export default function Login() {
+export default function Register() {
   const [username, onChangeUserName] = React.useState("");
   const [password, onChangePassword] = React.useState("");
   const [confirmPswd, onChangeConfirmPswd] = React.useState("");
