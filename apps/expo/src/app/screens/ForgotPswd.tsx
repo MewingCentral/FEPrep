d2925f742a670568ce37d07494c10dae8f89cf4c
e4s70f3d3n <annalisa.vitulli@ucf.edu>
2024-03-24 08:50:37 -0400

    fix format errors (hopefully)

--- apps/expo/src/app/screens/ForgotPswd.tsx
+++ apps/expo/src/app/screens/ForgotPswd.tsx
@@ -1,28 +1,24 @@
 import React from "react";
-import { 
-    SafeAreaView,
-    StyleSheet,
-    Text 
-} from "react-native";
+import { SafeAreaView, StyleSheet, Text } from "react-native";
 
 export default function ForgotPswd() {
-    return (
-        <SafeAreaView style={styles.container}>
-            <Text style={styles.containerText}> Are we doing forgot password??</Text>
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.containerText}> Are we doing forgot password??</Text>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: "center",
-      backgroundColor: "#f8fafa",
-    },
-    containerText: {
-        fontSize: 14,
-        fontWeight: "500",
-        color: "#020817",
-        alignSelf: "center",
-      },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#f8fafa",
+  },
+  containerText: {
+    fontSize: 14,
+    fontWeight: "500",
+    color: "#020817",
+    alignSelf: "center",
+  },
 });
