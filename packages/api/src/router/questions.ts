4c7dd357d2a205bc9b1595566ee6797d6f59123b
Caleb Rivera <calebrossr@gmail.com>
2024-03-25 19:10:19 -0400

    Add questions crud

--- packages/api/src/router/questions.ts
+++ packages/api/src/router/questions.ts
@@ -1,7 +1,28 @@
-import { createTRPCRouter, publicProcedure } from "../trpc";
+import { z } from "zod";
+
+import { eq, questions } from "@feprep/db";
+import { CreateQuestionSchema, UpdateQuestionSchema } from "@feprep/validators";
+
+import { adminProcedure, createTRPCRouter, publicProcedure } from "../trpc";
 
 export const questionsRouter = createTRPCRouter({
   all: publicProcedure.query(({ ctx }) => {
     return ctx.db.query.questions.findMany();
   }),
+  delete: adminProcedure.input(z.number()).mutation(({ ctx, input }) => {
+    return ctx.db.delete(questions).where(eq(questions.id, input));
+  }),
+  create: adminProcedure
+    .input(CreateQuestionSchema)
+    .mutation(({ ctx, input }) => {
+      return ctx.db.insert(questions).values(input);
+    }),
+  update: adminProcedure
+    .input(UpdateQuestionSchema)
+    .mutation(({ ctx, input: { questionId, ...question } }) => {
+      return ctx.db
+        .update(questions)
+        .set(question)
+        .where(eq(questions.id, questionId));
+    }),
 });
