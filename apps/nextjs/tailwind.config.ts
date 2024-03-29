0cb965a45584851339d810e66b91c1012bafe4ea
Caleb Rivera <calebrossr@gmail.com>
2024-03-28 20:47:03 -0400

    Add uploadthing

--- apps/nextjs/tailwind.config.ts
+++ apps/nextjs/tailwind.config.ts
@@ -1,9 +1,10 @@
 import type { Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
+import { withUt } from "uploadthing/tw";
 
 import baseConfig from "@feprep/tailwind-config/web";
 
-export default {
+export default withUt({
   // We need to append the path to the UI package to the content array so that
   // those classes are included correctly.
   content: [...baseConfig.content, "../../packages/ui/**/*.{ts,tsx}"],
@@ -16,4 +17,4 @@ export default {
       },
     },
   },
-} satisfies Config;
+} satisfies Config);
