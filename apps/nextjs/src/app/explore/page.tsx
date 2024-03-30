d38615b16905fdb6442bea1bcb00d10807521812
Caleb Rivera <calebrossr@gmail.com>
2024-03-30 19:11:31 -0400

    Add useUser hook to access current user

--- apps/nextjs/src/app/explore/page.tsx
+++ apps/nextjs/src/app/explore/page.tsx
@@ -1,32 +1,38 @@
+import { validateRequest } from "@feprep/auth";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@feprep/ui/tabs";
 
+import { UserProvider } from "~/utils/user";
 import { Nav } from "./nav";
 import { QuestionsTab } from "./questions-tab";
 import { StudySetsTab } from "./study-sets-tab";
 
-export default function Explore() {
-  return (
-    <div className="mx-auto flex h-screen w-full max-w-screen-lg flex-col">
-      <Nav />
-      <Tabs
-        className="flex flex-1 flex-col p-6 lg:min-h-0"
-        defaultValue="questions"
-      >
-        <TabsList className="mb-8 self-start">
-          <TabsTrigger value="questions">Questions</TabsTrigger>
-          <TabsTrigger value="study">Study Sets</TabsTrigger>
-        </TabsList>
+export default async function Explore() {
+  const { user } = await validateRequest();
 
-        <TabsContent value="questions">
-          <QuestionsTab />
-        </TabsContent>
-        <TabsContent
-          className="flex flex-1 flex-col gap-4 md:min-h-0 lg:flex-row"
-          value="study"
+  return (
+    <UserProvider user={user}>
+      <div className="mx-auto flex h-screen w-full max-w-screen-lg flex-col">
+        <Nav />
+        <Tabs
+          className="flex flex-1 flex-col p-6 lg:min-h-0"
+          defaultValue="questions"
         >
-          <StudySetsTab />
-        </TabsContent>
-      </Tabs>
-    </div>
+          <TabsList className="mb-8 self-start">
+            <TabsTrigger value="questions">Questions</TabsTrigger>
+            <TabsTrigger value="study">Study Sets</TabsTrigger>
+          </TabsList>
+
+          <TabsContent value="questions">
+            <QuestionsTab />
+          </TabsContent>
+          <TabsContent
+            className="flex flex-1 flex-col gap-4 md:min-h-0 lg:flex-row"
+            value="study"
+          >
+            <StudySetsTab />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </UserProvider>
   );
 }
