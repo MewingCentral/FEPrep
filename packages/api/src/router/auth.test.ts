88df751f6a87586e9947a88ac10c891ea9cea463
dany <71732131+DanyPalma@users.noreply.github.com>
2024-03-30 21:45:45 -0400

    Update auth.test.ts

--- packages/api/src/router/auth.test.ts
+++ packages/api/src/router/auth.test.ts
@@ -2,7 +2,6 @@ import { expect, suite, test } from "vitest";
 
 suite("Authentication Routes", () => {
   test("testing", () => {
-      expect(Math.sqrt(4)).toBe(2);
-    });
-
+    expect(Math.sqrt(4)).toBe(2);
+  });
 });
