edabc56fdaccbe8790978ec4577d11ebfdbc1d8b
Caleb Rivera <calebrossr@gmail.com>
2024-03-30 21:41:20 -0400

    Refactor session and user data retrieval in createTRPCContext()

--- packages/auth/src/validate-request.ts
+++ packages/auth/src/validate-request.ts
@@ -1,57 +1,63 @@
 import type { Session, User } from "lucia";
 import { cache } from "react";
-import { cookies } from "next/headers";
+import { cookies, headers } from "next/headers";
 
 import { lucia } from ".";
 
-export const uncachedValidateRequest = async ({
-  authorization,
-}: {
-  authorization: string | null;
-}): Promise<
+export const uncachedValidateRequest = async (): Promise<
   { user: User; session: Session } | { user: null; session: null }
 > => {
-  let sessionId = null;
+  const headersList = headers();
+  const source = headersList.get("x-trpc-source") ?? "unknown";
 
-  // Check if authorization header is present
-  if (authorization) {
-    sessionId = lucia.readBearerToken(authorization);
-  }
-  // If session ID is not found in authorization header, check cookies
-  else if (!sessionId) {
-    sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
-  }
+  if (source === "expo-react") {
+    const authorization = headersList.get("Authorization");
 
-  // If session ID is not found, return null
-  if (!sessionId) {
-    return { user: null, session: null };
-  }
-
-  const result = await lucia.validateSession(sessionId);
-
-  // Next.js throws an error when you attempt to set cookie when rendering page
-  try {
-    if (result.session?.fresh) {
-      const sessionCookie = lucia.createSessionCookie(result.session.id);
-      cookies().set(
-        sessionCookie.name,
-        sessionCookie.value,
-        sessionCookie.attributes,
-      );
+    if (!authorization) {
+      return { user: null, session: null };
     }
-    if (!result.session) {
-      const sessionCookie = lucia.createBlankSessionCookie();
-      cookies().set(
-        sessionCookie.name,
-        sessionCookie.value,
-        sessionCookie.attributes,
-      );
-    }
-  } catch {
-    console.error("Failed to set session cookie");
-  }
 
-  return result;
+    const sessionId = lucia.readBearerToken(authorization);
+
+    if (!sessionId) {
+      return { user: null, session: null };
+    }
+
+    const result = await lucia.validateSession(sessionId);
+    return result;
+  } else {
+    const cookiesList = cookies();
+    const sessionId = cookiesList.get(lucia.sessionCookieName)?.value ?? null;
+
+    if (!sessionId) {
+      return { user: null, session: null };
+    }
+
+    const result = await lucia.validateSession(sessionId);
+
+    try {
+      if (result.session?.fresh) {
+        const sessionCookie = lucia.createSessionCookie(result.session.id);
+        cookiesList.set(
+          sessionCookie.name,
+          sessionCookie.value,
+          sessionCookie.attributes,
+        );
+      }
+      if (!result.session) {
+        const sessionCookie = lucia.createBlankSessionCookie();
+        cookiesList.set(
+          sessionCookie.name,
+          sessionCookie.value,
+          sessionCookie.attributes,
+        );
+      }
+    } catch {
+      console.error("Failed to set session cookie");
+    }
+
+    return result;
+  }
 };
 
 export const validateRequest = cache(uncachedValidateRequest);
