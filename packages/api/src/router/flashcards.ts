61d1db681513c08cfd88e707ec20ae9a8417107e
dany <71732131+DanyPalma@users.noreply.github.com>
2024-03-31 00:49:11 -0400

    Create flashcards.ts

---
+++ packages/api/src/router/flashcards.ts
@@ -0,0 +1,50 @@
+// import { TRPCError } from "@trpc/server";
+
+import { and, eq, flashcardPacks, flashcards } from "@feprep/db";
+import { FlashCardPackSchema, FlashCardSchema } from "@feprep/validators";
+
+import { createTRPCRouter, publicProcedure } from "../trpc";
+
+export const flashcardsRouter = createTRPCRouter({
+  createPack: publicProcedure
+    .input(FlashCardPackSchema)
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db.insert(flashcardPacks).values({
+        name: input.name,
+        userId: input.userId,
+      });
+
+      const results = await ctx.db
+        .select({ id: flashcardPacks.id })
+        .from(flashcardPacks)
+        .where(eq(flashcardPacks.name, input.name));
+
+      const id = results[0];
+
+      return { newPackId: id };
+    }),
+
+  createCard: publicProcedure
+    .input(FlashCardSchema)
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db.insert(flashcards).values({
+        packId: input.packId,
+        front: input.front,
+        back: input.back,
+      });
+
+      const results = await ctx.db
+        .select({ id: flashcards.id })
+        .from(flashcards)
+        .where(
+          and(
+            eq(flashcards.front, input.front),
+            eq(flashcards.packId, input.packId),
+          ),
+        );
+
+      const id = results[0];
+
+      return { newCardId: id };
+    }),
+});
