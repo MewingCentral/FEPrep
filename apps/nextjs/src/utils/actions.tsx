2908207089cf389388bc3566cc8cfda573b70902
Caleb Rivera <calebrossr@gmail.com>
2024-04-02 06:14:53 -0400

    Add signOutAction function to utils/actions.tsx

---
+++ apps/nextjs/src/utils/actions.tsx
@@ -0,0 +1,21 @@
+"use server";
+
+import { cookies } from "next/headers";
+
+import { lucia, validateRequest } from "@feprep/auth";
+
+export async function signOutAction() {
+  const { session } = await validateRequest();
+  if (!session) {
+    return {
+      error: "No session found",
+    };
+  }
+  await lucia.invalidateSession(session.id);
+  const sessionCookie = lucia.createBlankSessionCookie();
+  cookies().set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes,
+  );
+}
