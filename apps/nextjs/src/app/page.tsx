57118fd64362ccc4abd50b11344597bded618f47
Caleb Rivera <calebrossr@gmail.com>
2024-04-02 05:37:04 -0400

    Clean up home page

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 
 import { Button } from "@feprep/ui/button";
@@ -11,19 +10,13 @@ export default async function HomePage() {
   return (
     <div className="flex flex-1 flex-col items-start justify-center">
       <div className="ml-12">
-        <Image
-          src="/mew.png"
-          width={120}
-          height={120}
-          alt="FEPrep Logo, :3 doggy"
-          className="mb-8 rounded-md shadow-md"
-        />
-        <h1 className="mb-4  text-left text-6xl font-semibold">FEPrep</h1>
-        <h2 className="mb-4  max-w-sm text-left text-3xl font-normal leading-normal">
+        <div className="mb-8 text-9xl font-bold">:3</div>
+        <h1 className="mb-4 text-left text-6xl font-semibold">FEPrep</h1>
+        <h2 className="mb-4 max-w-sm text-left text-3xl font-normal leading-normal">
           A new way to prepare for the Foundation Exam.
         </h2>
         <div className="flex flex-row gap-5">
-          <Link href="/problems/page">
+          <Link href="/explore">
             <Button type="submit">Start Practicing</Button>
           </Link>
           <Link href="/sign-up">
