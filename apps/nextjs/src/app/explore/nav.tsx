42b3dc617189e00bd7346a4cef907f92a496713f
Caleb Rivera <calebrossr@gmail.com>
2024-04-02 06:15:02 -0400

    Refactor explore page and nav component

--- apps/nextjs/src/app/explore/nav.tsx
+++ apps/nextjs/src/app/explore/nav.tsx
@@ -1,15 +1,13 @@
-"use client";
-
 import Image from "next/image";
 import Link from "next/link";
 
+import type { User } from "@feprep/auth";
+import { Button } from "@feprep/ui/button";
 import { Separator } from "@feprep/ui/separator";
 
-import { useUser } from "~/utils/user";
+import { signOutAction } from "~/utils/actions";
 
-export function Nav() {
-  const user = useUser();
-
+export async function Nav({ user }: { user: User | null }) {
   return (
     <nav className="flex h-16 items-center justify-between px-6">
       <div className="mr-5 flex h-8 items-center gap-3">
@@ -21,7 +19,22 @@ export function Nav() {
         <Link href="/dashboard">Dashboard</Link>
       </div>
 
-      {user && <div>Welcome, {user.email}</div>}
+      <div className="flex items-center">
+        {user ? (
+          <div className="flex items-center gap-4">
+            <span>Welcome, {user.email}</span>
+            <form action={signOutAction}>
+              <Button size="sm" type="submit">
+                Sign Out
+              </Button>
+            </form>
+          </div>
+        ) : (
+          <Link href="/sign-in" passHref>
+            <Button>Sign In</Button>
+          </Link>
+        )}
+      </div>
     </nav>
   );
 }
