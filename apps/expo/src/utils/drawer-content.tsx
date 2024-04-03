4c41991ee97bfce02d841a16786ad270e57721a5
anna-sage-local <anna.macinnis109@gmail.com>
2024-04-03 14:59:40 -0400

    logout api in drawer needs to be tested

--- apps/expo/src/utils/drawer-content.tsx
+++ apps/expo/src/utils/drawer-content.tsx
@@ -5,8 +5,27 @@ import { RadixIcon } from "radix-ui-react-native-icons";
 import { View, Pressable, Text } from "react-native";
 import Colors from "./colors";
 
+// api stuff for logging out.
+import { api } from "./api";
+import * as SecureStore from "expo-secure-store";
+
 export default function DrawerContent(props: any) {
     const router = useRouter();
+
+    const signOut = api.auth.signOut.useMutation({
+        onSuccess: () => {
+            SecureStore.setItem("session", "invalid");
+            router.push("../");
+        },
+        onError: (error) => {
+            // todo make this better??
+            console.log(error);
+        }
+    });
+
+    const onSignOut = () => {
+        signOut.mutate();
+    };
 
     return (
         <View style={{ flex: 1, backgroundColor: Colors.dark_bg, }}>
@@ -17,7 +36,7 @@ export default function DrawerContent(props: any) {
                 <DrawerItem label="Log out" 
                     labelStyle={{ color: Colors.dark_primary_text, marginLeft: -20, }}
                     icon={() => <RadixIcon name="exit" color={Colors.dark_primary_text} />}
-                    onPress={() => router.replace("/")} />
+                    onPress={onSignOut} />
                 <View style={{ height: 50, justifyContent: "center", }}>
                     <Text style={{textAlign: "center", color:Colors.dark_primary_text}}>todo darkmode toggle</Text>
                 </View>
