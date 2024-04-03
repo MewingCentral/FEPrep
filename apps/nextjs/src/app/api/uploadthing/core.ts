7bbc1a1625ab5f39c97f6c139f68ede8887ff23b
Caleb Rivera <calebrossr@gmail.com>
2024-04-02 23:40:44 -0400

    Update maxFileCount for pdfUploader

--- apps/nextjs/src/app/api/uploadthing/core.ts
+++ apps/nextjs/src/app/api/uploadthing/core.ts
@@ -9,7 +9,7 @@ const auth = async (_: Request) => ({ id: "fakeId" }); // Fake auth function
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
-  pdfUploader: f({ pdf: { maxFileSize: "512KB", maxFileCount: 2 } })
+  pdfUploader: f({ pdf: { maxFileSize: "512KB", maxFileCount: 1 } })
     // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
       // This code runs on your server before upload
