023bacae07f0f60089264a9fdf30827b133f4443
Caleb Rivera <calebrossr@gmail.com>
2024-04-02 23:40:31 -0400

    Disable canvas alias in webpack configuration

--- apps/nextjs/next.config.js
+++ apps/nextjs/next.config.js
@@ -22,6 +22,10 @@ const config = {
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
+  },
 };
 
 export default config;
