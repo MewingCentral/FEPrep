df5a81963ef418267ebf3049b485e214296f0c65
dany <71732131+DanyPalma@users.noreply.github.com>
2024-04-04 10:11:37 -0400

    Update comments.ts

--- packages/api/src/router/comments.ts
+++ packages/api/src/router/comments.ts
@@ -1,43 +1,40 @@
 import { z } from "zod";
 
-import { eq, comments } from "@feprep/db";
-
-import { CommentSchema, UpdateCommentSchema } from "@feprep/validators"
+import { comments, eq } from "@feprep/db";
+import { CommentSchema, UpdateCommentSchema } from "@feprep/validators";
 
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const commentsRouter = createTRPCRouter({
-
-    create: publicProcedure
-        .input(commentSchema)
-        .mutation(async ({ ctx, input }) => {
-            return await ctx.db.insert(comments).values({
-                questionId: input.questionId,
-                userId: input.userId,
-                content: input.content,
-            })
-            .returning();
-        }),
-    
-    read: publicProcedure
-        .input(z.number())
-        .mutation(async ({ ctx, input }) => {
-            return await ctx.db
-            .select()
-            .from(comments)
-            .where(eq(comments.questionId, input));
-        }),
-
-    update: publicProcedure
-        .input(UpdateCommentSchema)
-        .mutation(async ({ctx, input: { id, ...comment}}) => {
-            return await ctx.db.update(comments).set(comment).where(eq(comments.id, id));
-        }),
-
-    delete: publicProcedure
-        .input(z.number())
-        .mutation(async ({ ctx, input }) => {
-            return await ctx.db.delete(comments).where(eq(comments.id, input));
+  create: publicProcedure
+    .input(CommentSchema)
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.db
+        .insert(comments)
+        .values({
+          questionId: input.questionId,
+          userId: input.userId,
+          content: input.content,
         })
+        .returning();
+    }),
 
+  read: publicProcedure.input(z.number()).query(({ ctx, input }) => {
+    return ctx.db.query.comments.findMany({
+      where: eq(comments.id, input),
+    });
+  }),
+
+  update: publicProcedure
+    .input(UpdateCommentSchema)
+    .mutation(async ({ ctx, input: { id, ...comment } }) => {
+      return await ctx.db
+        .update(comments)
+        .set(comment)
+        .where(eq(comments.id, id));
+    }),
+
+  delete: publicProcedure.input(z.number()).mutation(async ({ ctx, input }) => {
+    return await ctx.db.delete(comments).where(eq(comments.id, input));
+  }),
 });
