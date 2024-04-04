0da2f8878eb880a6bc6c66200be57510fb7731cd
qnbby <quan.nguyen.duc.3010@gmail.com>
2024-04-04 14:18:28 -0400

    right tab done i fink TODO: api parameters to display errythin

--- apps/nextjs/src/app/problems/components/notes-tab.tsx
+++ apps/nextjs/src/app/problems/components/notes-tab.tsx
@@ -1,28 +1,11 @@
 "use client";
-
-import { Skeleton } from "@feprep/ui/skeleton";
-import type { RouterOutputs } from "@feprep/api";
-import { api } from "~/trpc/react";
-
-export function NotesTab({
-    data,
-}: {
-    data: RouterOutputs["questions"]["all"]; 
-    // change to notes content from specific user
-}
-
-) {
-    const getAllQuestions = api.questions.all.useQuery();
-  
-    // TODO: make this a lot nicer
-    // if (getAllQuestions.isLoading) {
-    //   return <Skeleton className="h-40 w-full" />;
-    // }
-  
+import { Textarea } from "@feprep/ui/textarea"
+export function NotesTab()
+{
     return (
-        <div className="w-full h-40"> 
-            <input className="w-full h-40 break-words" type="text" placeholder="Type here..." /> 
+        <div className="w-full h-full"> 
+            <Textarea className="h-40"/>
         </div>
     );
-  }
+}
   
---
+++ apps/nextjs/src/app/problems/components/resources-tab.tsx
@@ -0,0 +1,30 @@
+// "use client";
+import { Card, CardContent} from "@feprep/ui/card";
+// import { Label } from "@feprep/ui/label"
+// import { Textarea } from "@feprep/ui/textarea"
+// import { AvatarIcon } from "@feprep/ui";
+// import { Button } from "@feprep/ui/button";
+
+export function ResourcesTab(){
+    return (
+        <Card className="h-full flex-col">
+            
+            {/* public comments */}
+            <CardContent className="space-y-2 p-4">
+                <ResourcesLink link="http://google.com" />
+                <ResourcesLink link="https://www.altcademy.com/blog/how-to-make-text-a-link-in-reactjs/" />
+            </CardContent>
+        </Card>
+    );
+}
+
+function ResourcesLink({link}:
+    {
+        link: string;
+    }) {
+    return(
+        <Card className="p-2">        
+            <a className="p-3" href={link} target="_blank" rel="noopener noreferrer">{link}</a>
+        </Card>            
+    );
+}
