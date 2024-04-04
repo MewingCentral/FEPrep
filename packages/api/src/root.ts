dc35788decf4368a4350359f679c293cdbbf75d6
dany <71732131+DanyPalma@users.noreply.github.com>
2024-04-04 16:52:35 -0400

    Update root.ts

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -2,6 +2,7 @@ import { authRouter } from "./router/auth";
 import { commentsRouter } from "./router/comments";
 import { flashcardsRouter } from "./router/flashcards";
 import { questionsRouter } from "./router/questions";
+import { resourcesRouter } from "./router/resources";
 import { usersRouter } from "./router/users";
 import { createTRPCRouter, publicProcedure } from "./trpc";
 
@@ -12,6 +13,7 @@ export const appRouter = createTRPCRouter({
   questions: questionsRouter,
   flashcards: flashcardsRouter,
   comments: commentsRouter,
+  resources: resourcesRouter,
 });
 
 export type AppRouter = typeof appRouter;
