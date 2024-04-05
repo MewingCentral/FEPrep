924a5c438be12ccecca7d5fb2b82d9281d8af6e3
anna-sage-local <anna.macinnis109@gmail.com>
2024-04-04 23:58:40 -0400

    logout header icon working + styled

--- apps/expo/src/app/_layout.tsx
+++ apps/expo/src/app/_layout.tsx
@@ -1,5 +1,6 @@
 import { Stack, Slot } from "expo-router";
 import { Drawer } from "expo-router/drawer";
+import { useRouter } from "expo-router";
 import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 
@@ -13,10 +14,13 @@ import headerDefault from "~/utils/header-default";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 // import DrawerContent from "components/drawer-content";
 import DrawerContent from "./components/drawer-content";
+import { api } from "~/utils/api";
 
-import { StyleSheet } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import Colors from "~/utils/colors";
 import { AuthProvider } from "~/utils/auth";
+import { useAuth } from "~/utils/auth";
+import { RadixIcon } from "radix-ui-react-native-icons";
 
 // Drawer stuff
 const DrawerLayout = () => {
@@ -27,6 +31,7 @@ const DrawerLayout = () => {
 // It wraps your pages with the providers they need
 export default function RootLayout() {
   const { colorScheme } = useColorScheme();
+
   return (
     <AuthProvider>
       <TRPCProvider>
@@ -35,37 +40,50 @@ export default function RootLayout() {
           It also allows you to configure your screens 
         */}
         
-        <GestureHandlerRootView style={{ flex: 1 }}>
-          <Drawer drawerContent={DrawerContent}
-            screenOptions={headerDefault(colorScheme)}
-          >
-            <Drawer.Screen name="index" options={{
-              drawerItemStyle: { height: 0 },
-            }} />
-            <Drawer.Screen name="screens" options={{
-              headerShown: false,
-              drawerItemStyle: { height: 0 },
-            }} />
-            <Drawer.Screen name="dashboard" options={{
-              drawerItemStyle: { height: 0 },
-            }} />
-            <Drawer.Screen name="components/drawer-content" options={{
-              drawerItemStyle: { height: 0 },
-            }} />
-            <Drawer.Screen name="card-creation" options={{
-              headerShown: false,
-              drawerItemStyle: { height: 0 }
-            }} />
-          </Drawer>
-        </GestureHandlerRootView>
-        {/* <Stack screenOptions={headerDefault(colorScheme)}>
+        <Stack screenOptions={headerDefault(colorScheme)}>
+        <Stack.Screen
+        name="dashboard"
+        options={{
+          ...headerDefault(colorScheme),
+          headerRight: () => {
+            const { sessionId, setSessionId } = useAuth();
+            console.log("Session id in drawer!", sessionId)
+            const router = useRouter();
+          
+            const signOut = api.auth.signOut.useMutation({
+                onSuccess: () => {
+                    setSessionId("invalid")
+                    router.push("/");
+                },
+                onError: (error) => {
+                    // todo make this better??
+                    console.log(error);
+                }
+            });
+          
+            const onSignOut = () => {
+                signOut.mutate();
+            };
+
+            return (
+              <View>
+                {
+                  sessionId !== "invalid" && (
+                  <Pressable style={[styles.logoutContainer]} onPress={onSignOut}>
+                    <RadixIcon name="exit" color={Colors.dark_primary_text} />
+                  </Pressable> )
+                }
+              </View>
+            );
+          },
+        }} />
         <Stack.Screen
           name="card-creation"
           options={{
             headerShown: false,
           }}
         />
-      </Stack> */}
+      </Stack>
         <StatusBar />
       </TRPCProvider>
     </AuthProvider>
@@ -74,25 +92,16 @@ export default function RootLayout() {
 }
 
 const styles = StyleSheet.create({
-  drawerContainer: {
-    backgroundColor: Colors.dark_bg,
+  logoutContainer: {
+    flex: 1,
+    flexDirection: "row",
+    gap: 5,
+    paddingHorizontal: 5,
+    paddingVertical: 3,
   },
+  // logoutText: {
+  //   alignSelf: "center",
+  //   color: Colors.dark_primary_text,
+  //   textAlign: "center",
+  // },
 });
-
-
-// Makes screen white
-// export default function RootLayout() {
-//   const { colorScheme } = useColorScheme();
-//   return (
-//     <TRPCProvider>
-//       <GestureHandlerRootView>
-//         <Drawer screenOptions={headerDefault(colorScheme)}>
-//           <Drawer.Screen name="card-creation"
-//             options={{
-//               headerShown: false,
-//             }} />
-//         </Drawer>
-//       </GestureHandlerRootView>
-//     </TRPCProvider>
-//   );
-// }
--- apps/expo/src/app/dashboard/_layout.tsx
+++ apps/expo/src/app/dashboard/_layout.tsx
@@ -2,6 +2,7 @@ import { Stack } from "expo-router";
 import { Drawer } from "expo-router/drawer";
 import { StatusBar } from "expo-status-bar";
 import { useColorScheme } from "nativewind";
+import { Button } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import { TRPCProvider } from "~/utils/api";
@@ -17,13 +18,8 @@ export default function DashboardLayout() {
             The Stack component displays the current page.
             It also allows you to configure your screens 
           */}
-      {/* <GestureHandlerRootView>
-        <Drawer>
-          <Drawer.Screen name="(tabs)" options={{
-            headerShown: false,
-          }} />
-        </Drawer>
-      </GestureHandlerRootView> */}
+      {/* <Stack screenOptions={headerDefault(colorScheme)}> */}
+      {/* didn't work */}
       <Stack screenOptions={headerDefault(colorScheme)}>
         <Stack.Screen
           name="(tabs)"
