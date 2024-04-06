5463b5dd5cc848ed5acd3cacfa74f906036d722a
anna-sage-local <anna.macinnis109@gmail.com>
2024-04-06 14:48:09 -0400

    format fixed again

--- apps/expo/src/app/screens/Login.tsx
+++ apps/expo/src/app/screens/Login.tsx
@@ -18,15 +18,15 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Controller, useForm } from "react-hook-form";
 
 import { api } from "~/utils/api";
+import { useAuth } from "~/utils/auth";
 import screenStyles from "~/utils/screen-styles";
 import {
   SignInInput,
   SignInSchema,
 } from "../../../../../packages/validators/src";
-import { useAuth } from "~/utils/auth";
 
 export default function Login() {
-  const { setSessionId} = useAuth()
+  const { setSessionId } = useAuth();
   const router = useRouter();
   const {
     control,
