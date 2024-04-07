e58c0a89a85b2233aec61bf0d7019ee3c31a350e
e4s70f3d3n <annalisa.vitulli@ucf.edu>
2024-04-07 13:55:36 -0400

    change split style of landing page from flex to grid

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -8,9 +8,9 @@ export const runtime = "edge";
 
 export default async function HomePage() {
   return (
-    <div className="flex flex-row flex-cols-2 items-start justify-center">
-      <div className="ml-12">
-        <h1 className="mb-4 text-left text-6xl font-semibold">FEPrep</h1>
+    <div className="grid gap-y-8 grid-rows-2 muted-foreground">
+      <div className="ml-12 muted-foreground">
+        <h1 className="mb-4 text-left text-4xl font-semibold">FEPrep</h1>
         <h2 className="mb-4 max-w-sm text-left text-3xl font-normal leading-normal">
           A new way to prepare for the Foundation Exam.
         </h2>
@@ -21,12 +21,16 @@ export default async function HomePage() {
           <Link href="/sign-up">
             <Button
               type="submit"
-              className="rounded-md border border-input bg-background text-accent-foreground transition-colors hover:bg-accent "
-            >
+              className="rounded-md border border-input bg-background text-accent-foreground transition-colors hover:bg-accent">
               Create Account
             </Button>
           </Link>
         </div>
+      </div>
+      <div>
+        <h2 className="mb-4 max-w-sm text-left text-3xl font-normal leading-normal">
+          random text to see if my idea works??
+        </h2>
       </div>
     </div>
   );
