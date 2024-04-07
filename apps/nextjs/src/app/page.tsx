caacb26c01541e78039dc0a091bb249c52374882
e4s70f3d3n <annalisa.vitulli@ucf.edu>
2024-04-07 13:05:41 -0400

    split page, center, and remove symbol

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -8,9 +8,8 @@ export const runtime = "edge";
 
 export default async function HomePage() {
   return (
-    <div className="flex flex-1 flex-col items-start justify-center">
+    <div className="flex flex-row flex-cols-2 items-start justify-center">
       <div className="ml-12">
-        <div className="mb-8 text-9xl font-bold">:3</div>
         <h1 className="mb-4 text-left text-6xl font-semibold">FEPrep</h1>
         <h2 className="mb-4 max-w-sm text-left text-3xl font-normal leading-normal">
           A new way to prepare for the Foundation Exam.
