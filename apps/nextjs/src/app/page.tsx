58e847e0e37814b9941584220bf613860fe4e2a2
e4s70f3d3n <annalisa.vitulli@ucf.edu>
2024-04-07 17:39:58 -0400

    add landing page with proper division for redesign

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,37 +1,32 @@
-import Link from "next/link";
+//import Link from "next/link";
+// https://tailwindui.com/components/marketing/sections/content-sections
+// //import { Button } from "@feprep/ui/button";
 
-import { Button } from "@feprep/ui/button";
+// import "@feprep/ui";
 
-import "@feprep/ui";
-
-export const runtime = "edge";
+// export const runtime = "edge";
 
 export default async function HomePage() {
   return (
-    <div className="grid gap-y-8 grid-rows-2 muted-foreground">
-      <div className="ml-12 muted-foreground">
-        <h1 className="mb-4 text-left text-4xl font-semibold">FEPrep</h1>
-        <h2 className="mb-4 max-w-sm text-left text-3xl font-normal leading-normal">
-          A new way to prepare for the Foundation Exam.
-        </h2>
-        <div className="flex flex-row gap-5">
-          <Link href="/explore">
-            <Button type="submit">Start Practicing</Button>
-          </Link>
-          <Link href="/sign-up">
-            <Button
-              type="submit"
-              className="rounded-md border border-input bg-background text-accent-foreground transition-colors hover:bg-accent">
-              Create Account
-            </Button>
-          </Link>
-        </div>
+
+    //<div className="bg-muted-foreground">
+    <section className="">
+    
+      <div className="grid h-screen grid-rows-5">
+        
+        <div className="bg-muted-foreground"></div>
+      
+        <div className="bg-muted-foreground"></div>
+
+        <div className="bg-muted-foreground"></div>
+      
+        <div className="bg-white"></div>
+
+        <div className="bg-white"></div>
+        
       </div>
-      <div>
-        <h2 className="mb-4 max-w-sm text-left text-3xl font-normal leading-normal">
-          random text to see if my idea works??
-        </h2>
-      </div>
-    </div>
+
+    </section>
+
   );
 }
