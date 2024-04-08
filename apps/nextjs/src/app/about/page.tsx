3497e8f407274b8292f91c1179ab4bd840fb925d
e4s70f3d3n <annalisa.vitulli@ucf.edu>
2024-04-08 04:29:49 -0400

    add header statement to about us page

--- apps/nextjs/src/app/about/page.tsx
+++ apps/nextjs/src/app/about/page.tsx
@@ -8,10 +8,16 @@ export default async function AboutUs() {
     return (
         <section className="">
             <Nav user={user} />
-            <div className="grid h-screen grid-rows-2">
-                <div className="bg-lime-400"></div>
-                <div className="bg-pink-500"></div>
+            <div className="py-4 mb-4 space-y-16">
+                <h2 className="font-heading text-black text-4xl font-semibold text-foreground text-center">
+                    {"Meet the Developers"}
+                </h2>
             </div>
+
+                <div className="grid h-screen grid-rows-2">
+                    <div className="bg-lime-400"></div>
+                    <div className="bg-pink-500"></div>
+                </div>
         </section>
     );
 }
