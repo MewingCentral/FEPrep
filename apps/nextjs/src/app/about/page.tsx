1a77efc63ac7b104f58f9c855c6af3c98719e56a
e4s70f3d3n <annalisa.vitulli@ucf.edu>
2024-04-08 04:12:29 -0400

    update the structure of the rows after much debate (and research.. i'm tired)

--- apps/nextjs/src/app/about/page.tsx
+++ apps/nextjs/src/app/about/page.tsx
@@ -2,16 +2,15 @@ import { validateRequest } from "@feprep/auth";
 
 import { Nav } from "./nav";
 
-export default async function About() {
+export default async function AboutUs() {
     const { user } = await validateRequest();
   
     return (
         <section className="">
             <Nav user={user} />
-            <div className="grid h-screen grid-rows-3">
-                <div className="bg-green-400"></div>
-                <div className="bg-indigo-500"></div>
-                <div className="bg-yellow-300"></div>
+            <div className="grid h-screen grid-rows-2">
+                <div className="bg-lime-400"></div>
+                <div className="bg-pink-500"></div>
             </div>
         </section>
     );
