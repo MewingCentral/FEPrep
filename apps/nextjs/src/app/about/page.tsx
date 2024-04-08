09eed2622194c05f21672230e1ba91a06aeae454
e4s70f3d3n <annalisa.vitulli@ucf.edu>
2024-04-08 03:10:23 -0400

    change export of about function title

--- apps/nextjs/src/app/about/page.tsx
+++ apps/nextjs/src/app/about/page.tsx
@@ -2,13 +2,17 @@ import { validateRequest } from "@feprep/auth";
 
 import { Nav } from "./nav";
 
-export default async function Explore() {
+export default async function About() {
     const { user } = await validateRequest();
   
     return (
         <section className="">
             <Nav user={user} />
-
+            <div className="grid h-screen grid-rows-3">
+                <div className="bg-green-400"></div>
+                <div className="bg-indigo-500"></div>
+                <div className="bg-yellow-300"></div>
+            </div>
         </section>
     );
 }
