0c57b0d17d5c99fb8a600900254817c2a085a721
Pharit Smitasin <pharitsmitasin@gmail.com>
2024-04-09 19:12:41 -0400

    add scrollto bottom section and bottom content

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,14 +1,16 @@
+"use client";
+
 import Link from "next/link";
 
 import { Button } from "@feprep/ui/button";
 
 import "@feprep/ui";
 
-import { HeartIcon } from "@radix-ui/react-icons";
+import { ChevronDownIcon, HeartIcon } from "@radix-ui/react-icons";
 
 export const runtime = "edge";
 
-export default async function HomePage() {
+export default function HomePage() {
   return (
     <>
       <div className="flex min-h-screen flex-1 flex-col items-center justify-center">
@@ -31,18 +33,40 @@ export default async function HomePage() {
             </Link>
           </div>
         </div>
+        <div className="translate-y-60 transform">
+          <ChevronDownIcon
+            className="mt-14 h-auto w-[40px] animate-bounce hover:cursor-pointer"
+            onClick={() => {
+              window.scrollTo({
+                top: document.body.scrollHeight,
+                behavior: "smooth",
+              });
+            }}
+          />
+        </div>
       </div>
-      <div className="flex flex-1 flex-col items-center justify-center">
-        <div>
-          <h2 className="mb-4 text-left text-3xl font-normal leading-normal text-red-500">
-            Made with <HeartIcon /> in central Florida.
-          </h2>
-          <div className="flex flex-row gap-5">
+      <div className="flex min-h-screen flex-1 flex-col items-center justify-center">
+        <div className="flex flex-row content-center items-center justify-center gap-4 text-3xl font-normal leading-normal text-red-500">
+          <h2>Made with</h2>
+          <Link href="/about">
+            <HeartIcon className="h-auto w-[40px] transition-all duration-500 ease-in-out hover:scale-110 hover:cursor-pointer hover:text-foreground" />
+          </Link>
+          <h2>in central Florida.</h2>
+
+          {/* <div className="flex flex-row gap-5">
             <Link href="/explore">
               <Button type="submit">Start Practicing</Button>
             </Link>
-          </div>
+          </div> */}
         </div>
+        <p className="mx-4 text-center lg:mx-16">
+          UCF strives to help every student unleash their greatest potential.
+          Through a competitive curriculum and high academic excellence upheld
+          by standardized tests like the Foundation Exam, students and alumni
+          from the College of Engineering and Computer Science are employed at
+          top companies across the nation. With this app, we aim to help you on
+          this journey as you reach for the stars.
+        </p>
       </div>
     </>
   );
