38b29c0afd104cd7d020c1ef2efe30e7155be670
Pharit Smitasin <pharitsmitasin@gmail.com>
2024-04-08 22:08:37 -0400

    delete notes tab, allow resources to be added, working fully

--- apps/nextjs/src/app/problems/[id]/_components/right.tsx
+++ apps/nextjs/src/app/problems/[id]/_components/right.tsx
@@ -1,26 +1,28 @@
 import type { RouterOutputs } from "@feprep/api";
-// import type { User } from "@feprep/auth";
-import { ChatBubbleIcon, FileIcon, PlusIcon, VideoIcon } from "@feprep/ui";
+import type { User } from "@feprep/auth";
+import { ChatBubbleIcon, PlusIcon, VideoIcon } from "@feprep/ui";
 import { Button } from "@feprep/ui/button";
-// import {
-//   Sheet,
-//   SheetContent,
-//   SheetDescription,
-//   SheetHeader,
-//   SheetTitle,
-//   SheetTrigger,
-// } from "@feprep/ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@feprep/ui/sheet";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@feprep/ui/tabs";
 
-// import { CreateQuestionForm } from "~/app/explore/create-question-form";
+import { AddResourceForm } from "./add-resource-form";
 import { DiscussionTab } from "./discussion-tab";
 import { NotesTab } from "./notes-tab";
 import { ResourcesTab } from "./resources-tab";
 
 export async function Right({
   question,
+  user,
 }: {
   question: NonNullable<RouterOutputs["questions"]["byId"]>;
+  user: User | null;
 }) {
   return (
     <div className="flex-none rounded-md border p-4 lg:basis-1/2">
@@ -31,55 +33,53 @@ export async function Right({
               <ChatBubbleIcon className="mr-2" />
               Discussion
             </TabsTrigger>
-            <TabsTrigger value="notes">
-              <FileIcon className="mr-2" />
-              Notes
-            </TabsTrigger>
             <TabsTrigger value="resources">
               <VideoIcon className="mr-2" />
               Resources
             </TabsTrigger>
           </TabsList>
-          <Button>
-            <PlusIcon className=" h-4 w-4" />
-            Add Resource
-          </Button>
+          {/* if the user is a teacher, then show the button to add resources */}
+          {user?.type === "Teacher" && (
+            <AddResourceButton question={question} />
+          )}
         </div>
 
         <TabsContent value="discussion">
           <DiscussionTab question={question} />
         </TabsContent>
-        <TabsContent value="notes" asChild>
+        <TabsContent value="notes">
           <NotesTab />
         </TabsContent>
-        <TabsContent value="resources" asChild>
+        <TabsContent value="resources">
           <ResourcesTab question={question} />
-          {/* <AddResourceButton user={user} /> */}
         </TabsContent>
       </Tabs>
     </div>
   );
 }
 
-// function AddResourceButton({ user }: { user: User }) {
-//   return (
-//     <Sheet>
-//       <SheetTrigger asChild>
-//         <Button>
-//           <PlusIcon className="mr-2 h-4 w-4" />
-//           Add Resource
-//         </Button>
-//       </SheetTrigger>
-//       <SheetContent className="w-[400px] sm:w-[540px]">
-//         <SheetHeader>
-//           <SheetTitle>Create Question</SheetTitle>
-//           <SheetDescription>
-//             Fill out the form below to create a new question to be added to the
-//             site!
-//           </SheetDescription>
-//         </SheetHeader>
-//         <CreateQuestionForm user={user} />
-//       </SheetContent>
-//     </Sheet>
-//   );
-// }
+function AddResourceButton({
+  question,
+}: {
+  question: NonNullable<RouterOutputs["questions"]["byId"]>;
+}) {
+  return (
+    <Sheet>
+      <SheetTrigger asChild className="text-xs">
+        <Button>
+          <PlusIcon className="mr-2 h-4 w-4" />
+          Add Resource
+        </Button>
+      </SheetTrigger>
+      <SheetContent className="w-[400px] sm:w-[540px]">
+        <SheetHeader>
+          <SheetTitle>Add Resource</SheetTitle>
+          <SheetDescription>
+            Add a link to a resource for this question.
+          </SheetDescription>
+        </SheetHeader>
+        <AddResourceForm question={question} />
+      </SheetContent>
+    </Sheet>
+  );
+}
