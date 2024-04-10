35ea5f234c65b75bc0ee7416667710f8a272a6a3
anna-sage-local <anna.macinnis109@gmail.com>
2024-04-10 10:29:02 -0400

    public vs private progress + register err msg style + updated pass reqs err msg

--- apps/expo/src/utils/colors.tsx
+++ apps/expo/src/utils/colors.tsx
@@ -3,6 +3,7 @@
 const Colors = {
   dark_bg: "#020817",
   dark_sec: "#1E293B",
+  dark_sec2: "#2e3b52",
   dark_accent: "#3a4961",
   dark_primary_text: "#F8FAFC",
   dark_secondary_text: "#94A3B8",
@@ -17,6 +18,8 @@ const Colors = {
   light_text: "#020817",
   light_subheader_text: "#6E727C",
   light_btn_text: "#FFFFFF",
+
+  error_text: "#a83232",
 };
 
 export default Colors;
