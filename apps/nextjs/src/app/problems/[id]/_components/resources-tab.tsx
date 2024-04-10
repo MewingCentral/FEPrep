e12b8ff713b2418792faf702c6a091e93cd297fd
Caleb Rivera <calebrossr@gmail.com>
2024-04-10 10:45:24 -0400

    Move add resources button deeper down tree

--- apps/nextjs/src/app/problems/[id]/_components/resources-tab.tsx
+++ apps/nextjs/src/app/problems/[id]/_components/resources-tab.tsx
@@ -1,18 +1,57 @@
 import type { RouterOutputs } from "@feprep/api";
+import { validateRequest } from "@feprep/auth";
+import { PlusIcon } from "@feprep/ui";
+import { Button } from "@feprep/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@feprep/ui/sheet";
 
 import { api } from "~/trpc/server";
+import { AddResourceForm } from "./add-resource-form";
 import { ResourcesList } from "./resources-list";
 
-export function ResourcesTab({
+export async function ResourcesTab({
   question,
 }: {
   question: NonNullable<RouterOutputs["questions"]["byId"]>;
 }) {
+  const { user } = await validateRequest();
   const resources = api.resources.allByQuestionId(question.id);
 
   return (
     <div className="flex flex-col gap-4">
       <ResourcesList promise={resources} question={question} />
+      {user && <AddResourceButton question={question} />}
     </div>
+  );
+}
+
+function AddResourceButton({
+  question,
+}: {
+  question: NonNullable<RouterOutputs["questions"]["byId"]>;
+}) {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button size="icon" className="absolute right-8 top-[34px]">
+          <PlusIcon />
+        </Button>
+      </SheetTrigger>
+      <SheetContent className="w-[400px] sm:w-[540px]">
+        <SheetHeader>
+          <SheetTitle>Add Resource</SheetTitle>
+          <SheetDescription>
+            Add a link to a resource for this question.
+          </SheetDescription>
+        </SheetHeader>
+        <AddResourceForm question={question} />
+      </SheetContent>
+    </Sheet>
   );
 }
--- apps/nextjs/src/app/problems/[id]/_components/right.tsx
+++ apps/nextjs/src/app/problems/[id]/_components/right.tsx
@@ -1,27 +1,14 @@
 import type { RouterOutputs } from "@feprep/api";
-import type { User } from "@feprep/auth";
-import { ChatBubbleIcon, PlusIcon, VideoIcon } from "@feprep/ui";
-import { Button } from "@feprep/ui/button";
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@feprep/ui/sheet";
+import { ChatBubbleIcon, VideoIcon } from "@feprep/ui";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@feprep/ui/tabs";
 
-import { AddResourceForm } from "./add-resource-form";
 import { DiscussionTab } from "./discussion-tab";
 import { ResourcesTab } from "./resources-tab";
 
 export async function Right({
   question,
-  user,
 }: {
   question: NonNullable<RouterOutputs["questions"]["byId"]>;
-  user: User | null;
 }) {
   return (
     <div>
@@ -37,10 +24,6 @@ export async function Right({
               Resources
             </TabsTrigger>
           </TabsList>
-          {/* if the user is a teacher, then show the button to add resources */}
-          {user?.type === "Teacher" && (
-            <AddResourceButton question={question} />
-          )}
         </div>
 
         <TabsContent value="discussion">
@@ -51,31 +34,5 @@ export async function Right({
         </TabsContent>
       </Tabs>
     </div>
-  );
-}
-
-function AddResourceButton({
-  question,
-}: {
-  question: NonNullable<RouterOutputs["questions"]["byId"]>;
-}) {
-  return (
-    <Sheet>
-      <SheetTrigger asChild>
-        <Button>
-          <PlusIcon className="mr-2 h-4 w-4" />
-          Add Resource
-        </Button>
-      </SheetTrigger>
-      <SheetContent className="w-[400px] sm:w-[540px]">
-        <SheetHeader>
-          <SheetTitle>Add Resource</SheetTitle>
-          <SheetDescription>
-            Add a link to a resource for this question.
-          </SheetDescription>
-        </SheetHeader>
-        <AddResourceForm question={question} />
-      </SheetContent>
-    </Sheet>
   );
 }
