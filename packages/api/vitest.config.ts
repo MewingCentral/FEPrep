81ae96ceb9e0f63236d8156417ff8cf766ee8df6
dany <71732131+DanyPalma@users.noreply.github.com>
2024-04-10 16:49:54 -0400

    Update vitest.config.ts

--- packages/api/vitest.config.ts
+++ packages/api/vitest.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    reporters: process.env.GITHUB_ACTIONS ? ["dot", "github-actions"] : ["dot"],
+    reporters: process.env.GITHUB_ACTIONS ? ["dot", "github-actions"] : ["verbose"],
   },
 });
