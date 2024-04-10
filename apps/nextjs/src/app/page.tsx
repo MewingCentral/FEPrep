f6afec2cead06af8b48359a97f906cc00244301b
Pharit Smitasin <pharitsmitasin@gmail.com>
2024-04-09 23:04:33 -0400

    update styling for bottom text and include links and ucf logo

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -7,7 +7,7 @@ import { Button } from "@feprep/ui/button";
 
 import "@feprep/ui";
 
-import { ChevronDownIcon, HeartIcon } from "@radix-ui/react-icons";
+import { ChevronDownIcon, HeartFilledIcon } from "@radix-ui/react-icons";
 
 export const runtime = "edge";
 
@@ -55,22 +55,38 @@ export default function HomePage() {
           />
         </div>
       </div>
-      <div className="flex min-h-screen flex-1 flex-col items-center justify-center">
-        <div className="flex flex-row content-center items-center justify-center gap-1 text-3xl font-normal leading-normal text-red-500">
-          <h2>Made with</h2>
+      <div className="flex min-h-screen flex-1 flex-col items-center justify-center gap-2">
+        <div className="flex flex-row content-center items-center justify-center gap-2 text-3xl font-normal leading-normal text-red-500">
+          <h2 className="font-semibold">Made with</h2>
           <Link href="/about">
-            <HeartIcon className="h-[30px] w-[30px] transition-all duration-500 ease-in-out hover:scale-110 hover:cursor-pointer hover:text-foreground" />
+            <HeartFilledIcon className="h-[45px] w-auto transition-all duration-500 ease-in-out hover:scale-110 hover:cursor-pointer hover:text-foreground" />
           </Link>
-          <h2>in central Florida.</h2>
+          <h2 className="font-semibold">in central Florida.</h2>
         </div>
         <p className="mx-4 w-3/4 text-center text-xl lg:mx-16">
-          UCF strives to help every student unleash their greatest potential.
+          <Link href="https://www.ucf.edu/" className="underline">
+            <b>UCF</b>
+          </Link>{" "}
+          strives to help every student unleash their greatest potential.
           Through a competitive curriculum and high academic excellence upheld
-          by standardized tests like the Foundation Exam, students and alumni
-          from the College of Engineering and Computer Science are employed at
-          top companies across the nation. With this app, we aim to help you on
-          this journey as you reach for the stars.
+          by standardized tests like the{" "}
+          <Link
+            href="https://www.cs.ucf.edu/ucf_section/foundation-exam/"
+            className="underline"
+          >
+            <b>Foundation Exam</b>
+          </Link>
+          , students and alumni from the College of Engineering and Computer
+          Science are employed at top companies across the nation. With this
+          app, we aim to help you on this journey as you reach for the stars.
         </p>
+        <Image
+          src="/ucf.svg"
+          className="mt-4 h-[120px] w-auto text-foreground dark:invert"
+          width={50}
+          height={50}
+          alt="UCF Logo"
+        />
       </div>
     </>
   );
