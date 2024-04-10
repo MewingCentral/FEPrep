f9e7f3d87a40c8349f92745acedb756fd7c49402
dany <71732131+DanyPalma@users.noreply.github.com>
2024-04-10 16:05:19 -0400

    Update root.ts

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,16 +1,10 @@
-import { authRouter } from "./router/auth";
 import { commentsRouter } from "./router/comments";
 import { flashcardsRouter } from "./router/flashcards";
-import { questionsRouter } from "./router/questions";
 import { resourcesRouter } from "./router/resources";
-import { usersRouter } from "./router/users";
 import { createTRPCRouter, publicProcedure } from "./trpc";
 
 export const appRouter = createTRPCRouter({
   hello: publicProcedure.query(() => "hewwo world"),
-  users: usersRouter,
-  auth: authRouter,
-  questions: questionsRouter,
   flashcards: flashcardsRouter,
   comments: commentsRouter,
   resources: resourcesRouter,
