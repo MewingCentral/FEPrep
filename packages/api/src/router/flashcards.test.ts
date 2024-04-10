c9f374c087291b683b2bbeb332900aea7430a446
dany <71732131+DanyPalma@users.noreply.github.com>
2024-04-10 16:06:29 -0400

    Update flashcards.test.ts

--- packages/api/src/router/flashcards.test.ts
+++ packages/api/src/router/flashcards.test.ts
@@ -1,48 +1,44 @@
 import type { inferProcedureInput } from "@trpc/server";
-    import { expect, suite, test } from "vitest";
-    import type { AppRouter } from "../root";
-    import { createCaller, createTRPCContext } from "../index";
+import { expect, suite, test } from "vitest";
+
+import type { AppRouter } from "../root";
+import { createCaller, createTRPCContext } from "../index";
 
 suite("Flashcards testing", () => {
+  const ctx = createTRPCContext({ headers: new Headers() });
+  const caller = createCaller(ctx);
 
-    const ctx = createTRPCContext({ headers: new Headers() });
-    const caller = createCaller(ctx);
+  test("createPack", async () => {
+    const input: inferProcedureInput<AppRouter["flashcards"]["createPack"]> = {
+      name: "testing pack",
+      userId: "wtroe4jemkmdjq4",
+    };
 
-    test("createPack", async () => {
+    const res = await caller.flashcards.createPack(input);
 
-       const input: inferProcedureInput<AppRouter["flashcards"]["createPack"]> = {
-        name:"testing pack",
-        userId:"wtroe4jemkmdjq4",
-       };
+    console.log(res);
 
-       const res = await caller.flashcards.createPack(input);
+    expect(res[0]).toBeDefined();
+    expect(res[0]).toHaveProperty("id");
+    expect(res[0]).toHaveProperty("name");
+    expect(res[0]).toHaveProperty("userId");
+  });
 
-       console.log(res);
+  test("createCard", async () => {
+    const input: inferProcedureInput<AppRouter["flashcards"]["createCard"]> = {
+      packId: 1,
+      front: "text front text",
+      back: "text back text",
+    };
 
-       expect(res[0]).toBeDefined();
-       expect(res[0]).toHaveProperty('id');
-       expect(res[0]).toHaveProperty('name');
-       expect(res[0]).toHaveProperty('userId');
+    const res = await caller.flashcards.createCard(input);
 
-    });
+    console.log(res);
 
-    test("createCard", async () => {
-
-       const input: inferProcedureInput<AppRouter["flashcards"]["createCard"]> = {
-        packId:1,
-        front:"text front text",
-        back:"text back text",
-       };
-
-       const res = await caller.flashcards.createCard(input);
-
-       console.log(res);
-
-       expect(res[0]).toBeDefined();
-       expect(res[0]).toHaveProperty('id');
-       expect(res[0]).toHaveProperty('packId');
-       expect(res[0]).toHaveProperty('front');
-       expect(res[0]).toHaveProperty('back');
-
-    });
+    expect(res[0]).toBeDefined();
+    expect(res[0]).toHaveProperty("id");
+    expect(res[0]).toHaveProperty("packId");
+    expect(res[0]).toHaveProperty("front");
+    expect(res[0]).toHaveProperty("back");
+  });
 });
