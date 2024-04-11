42821e96a89aa96af62b4a9b596f12157b2710fd
Pharit Smitasin <pharitsmitasin@gmail.com>
2024-04-10 21:42:15 -0400

    allow fullscreen for resource videos

--- apps/nextjs/src/app/problems/[id]/_components/resources-list.tsx
+++ apps/nextjs/src/app/problems/[id]/_components/resources-list.tsx
@@ -41,6 +41,7 @@ export function ResourcesList({
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 referrerPolicy="strict-origin-when-cross-origin"
                 key={resource.id}
+                allowFullScreen
               ></iframe>
             ))}
           </div>
