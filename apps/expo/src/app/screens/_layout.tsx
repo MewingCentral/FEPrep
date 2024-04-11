78d3e26e8343b70d44e944667ce47a6226a13d3c
anna-sage-local <anna.macinnis109@gmail.com>
2024-04-10 23:09:19 -0400

    fixed nid regex in all schema and forgot password working

--- apps/expo/src/app/screens/_layout.tsx
+++ apps/expo/src/app/screens/_layout.tsx
@@ -6,24 +6,6 @@ export default function Layout() {
   return (
     <Stack>
       <Stack.Screen
-        name="ValidateEmail"
-        options={{
-          presentation: "modal",
-          headerShown: false,
-          headerBackTitleVisible: false,
-          statusBarColor: Colors.dark_secondary_text,
-        }}
-      />
-      <Stack.Screen
-        name="VerificationModal"
-        options={{
-          presentation: "modal",
-          headerShown: false,
-          headerBackTitleVisible: false,
-          statusBarColor: Colors.dark_secondary_text,
-        }}
-      />
-      <Stack.Screen
         name="Login"
         options={{
           headerBackTitleVisible: false,
