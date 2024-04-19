81e9df480601dcb0eefb07a688abf88fb28e53ae
dany <71732131+DanyPalma@users.noreply.github.com>
2024-04-18 23:52:10 -0400

    Update right.tsx

--- apps/nextjs/src/app/problems/[id]/_components/right.tsx
+++ apps/nextjs/src/app/problems/[id]/_components/right.tsx
@@ -1,10 +1,18 @@
+import dynamic from "next/dynamic";
+
 import type { RouterOutputs } from "@feprep/api";
-import { ChatBubbleIcon, VideoIcon } from "@feprep/ui";
+import { ChatBubbleIcon, Pencil1Icon, VideoIcon } from "@feprep/ui";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@feprep/ui/tabs";
-import { Excalidraw } from "@excalidraw/excalidraw";
 
 import { DiscussionTab } from "./discussion-tab";
 import { ResourcesTab } from "./resources-tab";
+
+const ExcalidrawWithClientOnly = dynamic(
+  async () => (await import("./excalidraw-wrapper")).default,
+  {
+    ssr: false,
+  },
+);
 
 export async function Right({
   question,
@@ -12,34 +20,33 @@ export async function Right({
   question: NonNullable<RouterOutputs["questions"]["byId"]>;
 }) {
   return (
-    <div>
-      <Tabs defaultValue="resources">
-        <div className="mb-6 flex flex-row items-center justify-between gap-4">
-          <TabsList>
-            <TabsTrigger value="whiteboard">
-              Whiteboard
-            </TabsTrigger>
-            <TabsTrigger value="discussion">
-              <ChatBubbleIcon className="mr-2" />
-              Discussion
-            </TabsTrigger>
-            <TabsTrigger value="resources">
-              <VideoIcon className="mr-2" />
-              Resources
-            </TabsTrigger>
-          </TabsList>
-        </div>
+    <Tabs defaultValue="whiteboard">
+      <div className="mb-6 flex flex-row items-center justify-between gap-4">
+        <TabsList>
+          <TabsTrigger value="whiteboard">
+            <Pencil1Icon className="mr-2" />
+            Whiteboard
+          </TabsTrigger>
+          <TabsTrigger value="discussion">
+            <ChatBubbleIcon className="mr-2" />
+            Discussion
+          </TabsTrigger>
+          <TabsTrigger value="resources">
+            <VideoIcon className="mr-2" />
+            Resources
+          </TabsTrigger>
+        </TabsList>
+      </div>
 
-        <TabsContent value="discussion">
-          <DiscussionTab question={question} />
-        </TabsContent>
-        <TabsContent value="resources">
-          <ResourcesTab question={question} />
-        </TabsContent>
-        <TabsContent value="whiteboard">
-          <Excalidraw />
-        </TabsContent>
-      </Tabs>
-    </div>
+      <TabsContent value="discussion">
+        <DiscussionTab question={question} />
+      </TabsContent>
+      <TabsContent value="resources">
+        <ResourcesTab question={question} />
+      </TabsContent>
+      <TabsContent value="whiteboard" className="h-[70vh]">
+        <ExcalidrawWithClientOnly />
+      </TabsContent>
+    </Tabs>
   );
 }
