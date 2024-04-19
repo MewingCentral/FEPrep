7fe6d584c7b4feac7c12ce1dd45bb962e6672ab8
dany <71732131+DanyPalma@users.noreply.github.com>
2024-04-19 00:26:21 -0400

    Update excalidraw-wrapper.tsx

--- apps/nextjs/src/app/problems/[id]/_components/excalidraw-wrapper.tsx
+++ apps/nextjs/src/app/problems/[id]/_components/excalidraw-wrapper.tsx
@@ -1,11 +1,29 @@
 "use client";
 
 import { Excalidraw } from "@excalidraw/excalidraw";
+import { useTheme } from "next-themes";
+
+
+
+
 
 export default function ExcalidrawWrapper() {
+
+  const { resolvedTheme } = useTheme();
+
+  const checkDarkTheme =
+    typeof window !== "undefined" &&
+    (resolvedTheme === "dark" ||
+      (window.matchMedia("(prefers-color-scheme: dark)").matches &&
+        resolvedTheme === "system"));
+
   return (
-    <div className="h-full w-full">
-      <Excalidraw />
+    <div className="h-full w-full border-2 border-accent">
+    <Excalidraw
+      initialData={{
+        appState: { theme: checkDarkTheme ? 'dark' : 'light' }
+      }} 
+      />
     </div>
   );
 }
